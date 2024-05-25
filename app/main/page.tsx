2282c9de01f0d3762db6fd84d6eea001abe54636
78-artilleryman <ywy040150@gmail.com>
2024-05-25 18:52:36 +0900

    💄 Design: main 페이지 레이아웃 설정
    design: main 페이지 레이아웃 설정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -3,9 +3,14 @@ import SideBar from "./_components/SideBar/SideBar";
 
 function page() {
   return (
-    <div>
+    <main className="m-0 mx-auto flex w-[1440px] flex-row">
       <SideBar />
-    </div>
+      <div>
+        <h1>프로젝트</h1>
+        <div>정렬 필터 & 검색바</div>
+        <div>프로젝트 리스트</div>
+      </div>
+    </main>
   );
 }
 
