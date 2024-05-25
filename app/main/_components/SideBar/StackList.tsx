a801d537d79bddb7c44447a6566394de44231fce
78-artilleryman <ywy040150@gmail.com>
2024-05-25 20:57:14 +0900

    ✨ Feat: 사이드바 토글 기능 추가
    feat: 사이드바 토글 기능 추가

--- app/main/_components/SideBar/StackList.tsx
+++ app/main/_components/SideBar/StackList.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { StackListType } from "@/app/_types/StackType";
 import topArrowIcon from "@/public/icons/topArrow.svg";
 import bottomArrowIcon from "@/public/icons/bottomArrow.svg";
+import useToggleHook from "@/app/_hooks/useToggleHook";
 import StackItem from "./StackItem";
 
 interface StackListProps {
@@ -11,21 +13,27 @@ interface StackListProps {
 }
 
 function StackList({ stackDatas, title }: StackListProps) {
+  const { isOpen, toggleState } = useToggleHook();
+
   return (
     <div className="mt-5 w-full">
-      <div className="flex w-full flex-row items-center gap-1 py-2">
+      <div className="flex w-full cursor-pointer flex-row items-center gap-1 py-2" onClick={toggleState}>
         <h2 className="text-base font-semibold">{title}</h2>
         <button>
-          <Image src={topArrowIcon} alt="위로 향한 화살표 버튼." width={20} height={20} />
+          {isOpen ? (
+            <Image src={topArrowIcon} alt="위로 향한 화살표 버튼." width={20} height={20} />
+          ) : (
+            <Image src={bottomArrowIcon} alt="아래로 향한 화살표 버튼." width={20} height={20} priority />
+          )}
         </button>
-        <Image src={bottomArrowIcon} alt="아래로 향한 화살표 버튼." width={20} height={20} />
       </div>
       <ul className="mt-4 flex flex-col gap-2">
-        {stackDatas.map((data, i) => (
-          <StackItem key={i} image={data.iamge}>
-            {data.name}
-          </StackItem>
-        ))}
+        {isOpen &&
+          stackDatas.map((data, i) => (
+            <StackItem key={i} image={data.iamge}>
+              {data.name}
+            </StackItem>
+          ))}
       </ul>
     </div>
   );
