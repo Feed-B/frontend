7e509c0dbe996b498835091ed50e38d65ab644d6
78-artilleryman <ywy040150@gmail.com>
2024-05-25 16:58:37 +0900

    🎨 Style: 컨벤션에 맞게 코드 변경
    1

--- app/_components/Profile/Profile.tsx
+++ app/_components/Profile/Profile.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import profileIcon from "@/public/icons/profile.svg";
 
 function Profile() {
   return (
     <div className="custom-width flex h-10 w-10 flex-row items-center justify-center rounded-full bg-gray-200">
-      <Image src="/icons/profile.svg" alt="profile" width={20} height={20} priority />
+      <Image src={profileIcon} alt="profile" width={20} height={20} priority />
     </div>
   );
 }
