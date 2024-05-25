add37a20bf3b060929c55bd7d38517e6706aa0f9
codingaring <sksms2104@naver.com>
2024-05-26 00:36:54 +0900

    💄 Design : 정렬 및 gap 속성 추가

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -5,7 +5,7 @@ function OnlyHeader({ children }: PropsWithChildren) {
   return (
     <>
       <div className="bg-[#6b6b6b] py-6 text-center text-white">헤더</div>
-      {children}
+      <div className="ml-[50%] mt-10 flex w-[90rem] -translate-x-1/2 flex-col gap-10">{children}</div>
     </>
   );
 }
