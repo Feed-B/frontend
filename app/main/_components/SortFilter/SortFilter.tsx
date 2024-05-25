14465050d1f31cc465a7ba8f2709ec408bfbf4df
78-artilleryman <ywy040150@gmail.com>
2024-05-25 21:40:45 +0900

    💄 Design: 정렬 필터 UI 생성
    design: 정렬 필터 UI 생성

---
+++ app/main/_components/SortFilter/SortFilter.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { MouseEvent, useState } from "react";
+
+function SortFilter() {
+  const [SortState, setSortState] = useState<string>("");
+
+  const isChangeSort = (e: MouseEvent<HTMLButtonElement>) => {
+    setSortState(e.currentTarget.textContent || "");
+  };
+
+  return (
+    <div className="flex h-6 flex-row items-center gap-3">
+      <button
+        className={`border-r border-solid pr-2 text-base font-normal leading-4 ${SortState === "최신순" ? "text-black" : "text-gray-400"}`}
+        onClick={isChangeSort}>
+        최신순
+      </button>
+      <button
+        className={`border-r border-solid pr-2 text-base font-normal leading-4 ${SortState === "좋아요순" ? "text-black" : "text-gray-400"}`}
+        onClick={isChangeSort}>
+        좋아요순
+      </button>
+      <button
+        className={`text-base font-normal ${SortState === "조회순" ? "text-black" : "text-gray-400"}`}
+        onClick={isChangeSort}>
+        조회순
+      </button>
+    </div>
+  );
+}
+
+export default SortFilter;
