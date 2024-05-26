d98879866d566031e277e9afc147a460ae24b59d
codingaring <sksms2104@naver.com>
2024-05-26 23:39:58 +0900

    💄 Design : 카드 목록 없을 때 UI 구현

---
+++ app/mypage/_components/EmptyCard.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import emptyFileIcon from "@/public/icons/empty-file.svg";
+
+function EmptyCard() {
+  return (
+    <div className="absolute left-1/2 flex -translate-x-1/2  flex-col items-center justify-center gap-6">
+      <Image width={54} height={72} src={emptyFileIcon} alt="파일 없음" />
+      <p className="text-xl font-semibold text-[#8B8B8B]">프로젝트 목록이 없어요.</p>
+    </div>
+  );
+}
+
+export default EmptyCard;
