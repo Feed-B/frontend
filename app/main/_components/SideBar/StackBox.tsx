0f2d75be5f23f9f9c2f63147d0953f75855c60f6
78-artilleryman <ywy040150@gmail.com>
2024-05-26 22:56:36 +0900

    💄 Design: 시안에 맞게 스타일 수정
    design: 시안에 맞게 스타일 수정

--- app/main/_components/SideBar/StackBox.tsx
+++ app/main/_components/SideBar/StackBox.tsx
@@ -13,8 +13,8 @@ interface StackBoxProps {
 function StackBox({ stackDatas }: StackBoxProps) {
   const { stackState, setStackState, isDeleteStack } = useGetStack();
   return (
-    <div className="min-h-44 w-full  rounded-2xl bg-[#F8FAFB] p-3">
-      <ul className="flex flex-row  flex-wrap gap-2">
+    <div className="min-h-44 w-full rounded-2xl bg-[#F8FAFB] p-3">
+      <ul className="flex flex-row flex-wrap gap-2">
         {stackState.map((data, i) => {
           const stackItem = stackDatas.find(item => item.name === data);
           return (
@@ -27,7 +27,7 @@ function StackBox({ stackDatas }: StackBoxProps) {
       </ul>
       {stackState.length > 0 && (
         <div
-          className="flex h-7 w-20 cursor-pointer flex-row items-center gap-2 rounded-2xl border border-solid border-[#5177FF] bg-white px-2 py-1"
+          className="mt-3 flex h-7 w-20 cursor-pointer flex-row items-center gap-2 rounded-2xl border border-solid border-[#5177FF] bg-white px-2 py-1"
           onClick={() => setStackState([])}>
           <p className="  text-sm font-normal text-[#5177FF]">초기화</p>
           <Image src={resetIcon} alt="기술스택 초기화 버튼입니다." width={16} />
