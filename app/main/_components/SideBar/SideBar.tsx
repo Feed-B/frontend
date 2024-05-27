dbcd5cd9d47c07fffaa78f216ca0bfcd42ba6e45
78-artilleryman <ywy040150@gmail.com>
2024-05-28 02:09:41 +0900

    🎨 Style: 상수명 변경
    FULL_STACK_DATE => FULL_STACK_DATA

--- app/main/_components/SideBar/SideBar.tsx
+++ app/main/_components/SideBar/SideBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { FRONT_END_STACK, BACK_END_STACK, FULL_STACK_DATE } from "@/app/_constants/StackData";
+import { FRONT_END_STACK, BACK_END_STACK, FULL_STACK_DATA } from "@/app/_constants/StackData";
 import StackProvider from "../../_context/StackProvider";
 import StackList from "./StackList";
 import StackBox from "./StackBox";
@@ -10,7 +10,7 @@ function SideBar() {
   return (
     <div className="mr-5 w-56">
       <StackProvider>
-        <StackBox stackDatas={FULL_STACK_DATE} />
+        <StackBox stackDatas={FULL_STACK_DATA} />
         <StackList title="프론트엔드" stackDatas={FRONT_END_STACK} />
         <StackList title="백엔드" stackDatas={BACK_END_STACK} />
       </StackProvider>
