6c6a40aaaf69fdd7fb6b5e07711fcf72e05eee24
78-artilleryman <ywy040150@gmail.com>
2024-05-27 14:51:37 +0900

    🎨 Style: 상수 변수명 변경
    style: 상수 변수명 변경

--- app/main/_components/SideBar/SideBar.tsx
+++ app/main/_components/SideBar/SideBar.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import React from "react";
-import { frontEndStack, backEndStack } from "@/app/_constants/StackData";
+import { FRONT_END_STACK, BACK_END_STACK } from "@/app/_constants/StackData";
 import StackProvider from "../../_context/StackProvider";
 import StackList from "./StackList";
 import StackBox from "./StackBox";
 
 function SideBar() {
-  const fullStackData = frontEndStack.concat(backEndStack);
+  const FULL_STACK_DATE = FRONT_END_STACK.concat(BACK_END_STACK);
 
   return (
     <div className="mr-5 w-56">
       <StackProvider>
-        <StackBox stackDatas={fullStackData} />
-        <StackList title="프론트엔드" stackDatas={frontEndStack} />
-        <StackList title="백엔드" stackDatas={backEndStack} />
+        <StackBox stackDatas={FULL_STACK_DATE} />
+        <StackList title="프론트엔드" stackDatas={FRONT_END_STACK} />
+        <StackList title="백엔드" stackDatas={FRONT_END_STACK} />
       </StackProvider>
     </div>
   );
