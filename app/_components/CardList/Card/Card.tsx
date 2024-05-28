8af1515e5f69317d1fa29d74cf2112f79021492c
codingaring <sksms2104@naver.com>
2024-05-28 22:14:36 +0900

    ♻️ 코드 리뷰 반영
    불필요한 return문 제거, 불필요한 fragment 삭제, Card 컴포넌트 prop 객 체 형태로 압축해서 전달

--- app/_components/CardList/Card/Card.tsx
+++ app/_components/CardList/Card/Card.tsx
@@ -2,27 +2,28 @@ import Image, { StaticImageData } from "next/image";
 import HoverCard from "./HoverCard";
 
 interface CardProp {
-  projectTitle: string;
-  subDescription: string;
-  titleImage: StaticImageData;
+  id: number;
+  titleImage: StaticImageData | string;
   stackList: string[];
   wishCount: number;
-  isFavorite: boolean;
-  key: number;
+  isWish: boolean;
+  projectName: string;
+  subDescription: string;
+  description: string;
 }
 
-function Card({ projectTitle, subDescription, titleImage, stackList, wishCount, isFavorite, key }: CardProp) {
+function Card({ project }: { project: CardProp }) {
   return (
-    <div key={key} className="flex flex-col gap-2.5">
+    <div className="flex flex-col gap-2.5">
       <div className="group relative min-h-[320px] min-w-[320px] overflow-hidden rounded-md">
         <div className="absolute inset-0 z-30 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-          <HoverCard isWishProject={isFavorite} wishCount={wishCount} stackList={stackList} />
+          <HoverCard isWishProject={project.isWish} wishCount={project.wishCount} stackList={project.stackList} />
         </div>
-        <Image fill src={titleImage} alt={subDescription} />
+        <Image fill src={project.titleImage} alt={project.subDescription} />
       </div>
       <div className="flex w-[21rem] flex-col gap-1">
-        <h6 className="overflow-hidden text-ellipsis whitespace-nowrap font-bold">{projectTitle}</h6>
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap">{subDescription}</p>
+        <h6 className="overflow-hidden text-ellipsis whitespace-nowrap font-bold">{project.projectName}</h6>
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap">{project.subDescription}</p>
       </div>
     </div>
   );
