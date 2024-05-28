935f84c47476117f14c820c9475070e3b373e79a
codingaring <sksms2104@naver.com>
2024-05-28 15:54:49 +0900

    💄 Design : 찜버튼 UI 구현

--- app/_components/CardList/Card/HoverCard.tsx
+++ app/_components/CardList/Card/HoverCard.tsx
@@ -1,6 +1,15 @@
+import WishCount from "../../WishCount/WishCount";
 import StackBadge from "./StackBadge";
 
-function HoverCard({ stackList }: { stackList: string[] }) {
+function HoverCard({
+  stackList,
+  wishCount,
+  isWishProject,
+}: {
+  stackList: string[];
+  wishCount: number;
+  isWishProject: boolean;
+}) {
   return (
     <div className="flex p-6">
       <div className="z-20">
@@ -9,7 +18,9 @@ function HoverCard({ stackList }: { stackList: string[] }) {
             return <StackBadge key={index} stack={stack} />;
           })}
         </div>
-        <div className="absolute bottom-5 right-5 text-white">찜하기</div>
+        <div className="absolute bottom-5 right-5 text-white">
+          <WishCount isFavorite={isWishProject} wishCount={wishCount} />
+        </div>
       </div>
       <div className="absolute inset-0 z-10 bg-gradient-to-t from-black to-[rgba(0,0,0,0)] opacity-90" />
     </div>
---
+++ app/_components/WishCount/WishCount.tsx
@@ -0,0 +1,12 @@
+import WishButton from "./WishButton/WishButton";
+
+function WishCount({ isFavorite = false, wishCount }: { isFavorite: boolean; wishCount: number }) {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <WishButton isFavorite={isFavorite} />
+      <p className="text-xl">{wishCount}</p>
+    </div>
+  );
+}
+
+export default WishCount;
