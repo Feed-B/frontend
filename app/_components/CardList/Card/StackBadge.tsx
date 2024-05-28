557ccab30182e9fa66f2921ed36ddc4c29f3eb76
codingaring <sksms2104@naver.com>
2024-05-28 16:01:07 +0900

    💄 Design : StackBadge 투명도 추가

--- app/_components/CardList/Card/StackBadge.tsx
+++ app/_components/CardList/Card/StackBadge.tsx
@@ -1,7 +1,8 @@
 function StackBadge({ key, stack }: { key: number; stack: string }) {
   return (
-    <div key={key} className="rounded-md bg-black px-5 py-2 text-center text-white">
-      {stack}
+    <div key={key} className="relative overflow-hidden rounded-md px-5 py-2">
+      <p className="text-center text-white">{stack}</p>
+      <div className="absolute inset-0 -z-10 bg-black opacity-70" />
     </div>
   );
 }
