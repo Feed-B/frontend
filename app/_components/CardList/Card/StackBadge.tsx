9fd28b3480c58c96ffc4d6e3f55e75e06982adeb
codingaring <sksms2104@naver.com>
2024-05-28 22:23:43 +0900

    ♻️ Refactor : 불필요한 key prop 제거

--- app/_components/CardList/Card/StackBadge.tsx
+++ app/_components/CardList/Card/StackBadge.tsx
@@ -1,6 +1,6 @@
-function StackBadge({ key, stack }: { key: number; stack: string }) {
+function StackBadge({ stack }: { stack: string }) {
   return (
-    <div key={key} className="relative overflow-hidden rounded-md px-5 py-2">
+    <div className="relative overflow-hidden rounded-md px-5 py-2">
       <p className="text-center text-white">{stack}</p>
       <div className="absolute inset-0 -z-10 bg-black opacity-70" />
     </div>
