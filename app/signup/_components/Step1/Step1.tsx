e5da4eec8597b0b0dc20111dc53bf7e781010f4f
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-05-29 16:33:35 +0900

    🎨 Style: 테일윈드에서 기본적으로 제공하는 속성으로 수정

--- app/signup/_components/Step1/Step1.tsx
+++ app/signup/_components/Step1/Step1.tsx
@@ -10,14 +10,14 @@ function Step1({ nextStep }: Step1Props) {
     <div className="mx-[auto] mt-[117px] flex max-w-[420px] flex-col items-center">
       <Image src={signUpBar1Icon} alt="signUpBar" />
 
-      <div className="mb-[90px] mt-[48px] text-[#3A3A3A]">닉네임과 소개를 작성해 주세요</div>
+      <div className="mb-[90px] mt-12 text-[#3A3A3A]">닉네임과 소개를 작성해 주세요</div>
 
       <div className="flex flex-col">
         <label htmlFor="nickName">닉네임*</label>
         <input
           type="text"
           id="nickName"
-          className="mb-[69px] h-[48px] w-[420px] rounded-[8px] border-[1px] border-[#d6d6d6] px-[14px] py-[16px] focus:border-[#0066DA] focus:outline-none"
+          className="mb-[69px] h-12 w-[420px] rounded-lg border border-[#d6d6d6] px-3.5 py-4 focus:border-[#0066DA] focus:outline-none"
         />
       </div>
 
@@ -26,11 +26,11 @@ function Step1({ nextStep }: Step1Props) {
         <textarea
           placeholder="자기를 어필할 수 있는 소개글을 작성해주세요 (150자)"
           id="introduction"
-          className="mb-[200px] h-[160px] w-[420px] rounded-[8px] border-[1px] border-[#d6d6d6] px-[14px] py-[16px] focus:border-[#0066DA] focus:outline-none"
+          className="mb-[200px] h-40 w-[420px] rounded-lg border border-[#d6d6d6] px-3.5 py-4 focus:border-[#0066DA] focus:outline-none"
         />
       </div>
 
-      <button onClick={nextStep} className="w-[118px] rounded-[8px] bg-[#0066DA] px-[29px] py-[12px] text-white">
+      <button onClick={nextStep} className="w-[118px] rounded-lg bg-[#0066DA] px-[29px] py-3 text-white">
         다음으로
       </button>
     </div>
