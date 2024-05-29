9925f94b6163bf6215501e0c9381a9198496ae0b
codingaring <sksms2104@naver.com>
2024-05-29 11:44:07 +0900

    🔧 Chore : tailwind.config.js에 screen 분기점 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,13 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    screens: {
+      mb: { max: "767px" },
+      tb: { min: "768px", max: "1199px" },
+      pc: { min: "1200px" },
     },
   },
   plugins: [],
