b0ccac6a621fa76be9838e1627b3be4da2465aa9
thgus5335 <thgus5335@naver.com>
2024-05-29 23:59:01 +0900

    :art: Style: 프로젝트 섹션 컴포넌트 분리
    Style: 프로젝트 섹션 컴포넌트 분리

---
+++ app/project/[projectId]/_components/Section/TeamMemberSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TeamMemberSection() {
+  return (
+    <section className="mt-10">
+      <p className="mb-4 text-lg font-bold">팀원</p>
+    </section>
+  );
+}
+
+export default TeamMemberSection;
