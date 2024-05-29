26f954206e81ca271185859094597acab3c8da40
78-artilleryman <ywy040150@gmail.com>
2024-05-29 23:40:40 +0900

    💄 Design: SearchBar 컴포넌트 디자인 변경
    design: SearchBar 컴포넌트 디자인 변경

--- app/main/_components/SearchBar/SearchBar.tsx
+++ app/main/_components/SearchBar/SearchBar.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import searchIcon from "@/public/icons/search.svg";
+import { SearchProject } from "./SearchAction";
 
 function SearchBar() {
-  // const SearchProject = async (formData: any) => {
-  //   "use server";
-
-  //   console.log(formData.get("search"));
-  // };
-
   return (
-    <div className="flex w-80 flex-row items-center gap-2 rounded-lg border border-solid border-blue-600 p-3">
-      <Image src={searchIcon} alt="검색 기능입니다." width={20} height={20} priority />
-      <form>
+    <div className="flex h-8 w-48 flex-row items-center gap-2 rounded-[20px] border border-solid border-[#D6D6D6] p-2">
+      <Image src={searchIcon} alt="검색 기능입니다." width={16} priority />
+      <form action={SearchProject}>
         <input
-          className="w-60 text-sm font-normal outline-none"
+          className="mb-1 w-36 text-xs font-normal outline-none"
           name="search"
-          placeholder="Search..."
+          placeholder="프로젝트 이름 검색하기"
           autoComplete="off"
         />
       </form>
