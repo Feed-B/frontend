a6858e09984336b089e03f96a0770ebf53b33f48
78-artilleryman <ywy040150@gmail.com>
2024-05-29 23:21:40 +0900

    💄 Design: SortFilter 디자인 변경
    design: SortFilter 디자인 변경

--- app/main/_components/SortFilter/SortFilter.tsx
+++ app/main/_components/SortFilter/SortFilter.tsx
@@ -10,19 +10,19 @@ function SortFilter() {
   };
 
   return (
-    <div className="mt-5 flex h-6 flex-row items-center gap-3">
+    <div className="flex h-6 flex-row items-center gap-3">
       <button
-        className={`border-r border-solid pr-2 text-base font-normal leading-4 ${SortState === "최신순" ? "text-black" : "text-gray-400"}`}
+        className={`border-r border-solid pr-2 text-lg font-bold leading-4 ${SortState === "최신순" ? "text-[#3F3F3F]" : "text-gray-400"}`}
         onClick={isChangeSort}>
         최신순
       </button>
       <button
-        className={`border-r border-solid pr-2 text-base font-normal leading-4 ${SortState === "좋아요순" ? "text-black" : "text-gray-400"}`}
+        className={`border-r border-solid pr-2 text-lg font-bold leading-4 ${SortState === "좋아요순" ? "text-[#3F3F3F]" : "text-gray-400"}`}
         onClick={isChangeSort}>
         좋아요순
       </button>
       <button
-        className={`text-base font-normal ${SortState === "조회순" ? "text-black" : "text-gray-400"}`}
+        className={`text-lg font-bold ${SortState === "조회순" ? "text-[#3F3F3F]" : "text-gray-400"}`}
         onClick={isChangeSort}>
         조회순
       </button>
