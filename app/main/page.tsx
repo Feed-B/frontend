aebe542839d016df86cdba70e84dc30a95a72b2c
78-artilleryman <ywy040150@gmail.com>
2024-05-29 23:51:03 +0900

    💄 Design: 컴포넌트간 간격 조절
    design: 컴포넌트간 간격 조절

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -5,7 +5,7 @@ function MainPage() {
   return (
     <main className="m-0 mx-auto grid w-[1200px] grid-cols-[230px_minmax(960px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
       <SelectStack />
-      <div className="col-start-2 row-start-2">프로젝트 리스트</div>
+      <div className="col-start-2 mt-8">프로젝트 리스트</div>
     </main>
   );
 }
