fbdcd28d68de0a6fa98b7a729f191509069dd6ab
78-artilleryman <ywy040150@gmail.com>
2024-05-31 00:42:37 +0900

    ✨ Feat: Stack Item 컴포넌트와 StackBox 컴포넌트 상호작용 기능 구현
    feat: Stack Item 컴포넌트와 StackBox 컴포넌트 상호작용 기능 구현

--- app/main/_components/SideBar/StackItem.tsx
+++ app/main/_components/SideBar/StackItem.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { useGetStack } from "@/app/main/_context/StackProvider";
 import { ImageType } from "@/app/_types/StackType";
+import useToggleHook from "@/app/_hooks/useToggleHook";
 
 interface StackItemProps {
   children: ReactNode;
@@ -11,10 +12,34 @@ interface StackItemProps {
 }
 
 function StackItem({ children, image }: StackItemProps) {
-  const { isChangeStack } = useGetStack();
+  const { stackState, isChangeStack, isDeleteStack } = useGetStack();
+  const { isOpen: stackClicked, toggleState, ChangecloseState, ChangeopenState } = useToggleHook();
+
+  const clickStack = () => {
+    toggleState();
+
+    if (stackClicked) {
+      isDeleteStack(children + "");
+    } else {
+      isChangeStack(children + "");
+    }
+  };
+
+  useEffect(() => {
+    const inStack = stackState.includes(children + "");
+
+    if (inStack) {
+      ChangeopenState();
+    } else {
+      ChangecloseState();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stackState]);
 
   return (
-    <li className="flex cursor-pointer flex-row items-center gap-2 p-2" onClick={() => isChangeStack(children + "")}>
+    <li
+      className={`flex w-40 cursor-pointer flex-row items-center gap-2 rounded-lg p-2 ${stackClicked ? "bg-[#F8FAFB]" : "opacity-50"}`}
+      onClick={clickStack}>
       <Image src={image} alt="기술스택입니다." width={20} />
       <p className="text-sm font-normal">{children}</p>
     </li>
