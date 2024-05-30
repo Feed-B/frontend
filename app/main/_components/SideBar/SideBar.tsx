eee3a7363ef04d018fa68fd3a821831d241495f2
78-artilleryman <ywy040150@gmail.com>
2024-05-30 13:40:40 +0900

    🎨 Style: 시맨틱 태그 적용
    style: 시맨틱 태그 적용

--- app/main/_components/SideBar/SideBar.tsx
+++ app/main/_components/SideBar/SideBar.tsx
@@ -6,10 +6,10 @@ import StackList from "./StackList";
 
 function SideBar() {
   return (
-    <div className="row-span-3 w-56">
+    <aside className="row-span-3 w-56">
       <StackList title="프론트엔드" stackDatas={FRONT_END_STACK} />
       <StackList title="백엔드" stackDatas={BACK_END_STACK} />
-    </div>
+    </aside>
   );
 }
 
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -3,9 +3,9 @@ import SelectStack from "./_components/SelectStack/SelectStack";
 
 function MainPage() {
   return (
-    <main className="m-0 mx-auto grid w-[1200px] grid-cols-[230px_minmax(960px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
+    <main className="mx-auto mt-[100px] grid w-[1200px] grid-cols-[230px_minmax(960px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
       <SelectStack />
-      <div className="col-start-2 mt-8">프로젝트 리스트</div>
+      <section className="col-start-2 mt-8">프로젝트 리스트</section>
     </main>
   );
 }
