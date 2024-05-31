008e0b67d1c1a03a00a823a8c466a86bbd5741f0
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-05-31 11:46:15 +0900

    ✨ Feat: useModal hook 추가

---
+++ app/_hooks/useModal.ts
@@ -0,0 +1,26 @@
+import { useRef, useState } from "react";
+import useHandleModalClick from "./useHandleModalClick";
+
+const useModal = () => {
+  const modalRef = useRef(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
+  useHandleModalClick(modalRef, handleModalClose);
+
+  return {
+    openModal,
+    modalRef,
+    handleModalOpen,
+    handleModalClose,
+  };
+};
+
+export default useModal;
