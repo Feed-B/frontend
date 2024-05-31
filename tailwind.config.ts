b5880a68f30404513e83343124c1f04356350ca6
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-05-31 11:43:45 +0900

    ✨ Feat: overlay color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        black: {
+          overlay: "rgba(0, 0, 0, 0.70)",
+        },
+      },
     },
     screens: {
       mb: { max: "767px" },
