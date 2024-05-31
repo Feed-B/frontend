6e5e8e7ea0459528db95d7b040debf9bd9e9633c
thgus5335 <thgus5335@naver.com>
2024-06-01 05:09:48 +0900

    :art: Style: UseStack 공통 컴포넌트로 분리
    Style: UseStack 공통 컴포넌트로 분리

---
+++ app/_components/Stack/UseStack.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import { FULL_STACK_DATA } from "@/app/_constants/StackData";
+
+interface StackListProps {
+  stackList: string[];
+}
+
+function UseStack({ stackList }: StackListProps) {
+  return (
+    <div className="rounded-xl border border-solid border-[#EBEBEB] bg-white p-3">
+      <ul className="flex flex-wrap gap-4">
+        {stackList.map(stack => {
+          const stackItem = FULL_STACK_DATA.find(item => item.name === stack);
+          return (
+            <li className="flex items-center gap-1" key={stackItem?.id}>
+              <Image src={stackItem?.image || ""} alt="기술 스택 이미지." width={20} />
+              <p className="text-xs">{stack}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default UseStack;
--- app/project/[projectId]/_components/Section/StackSection.tsx
+++ app/project/[projectId]/_components/Section/StackSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Image from "next/image";
-import { FULL_STACK_DATA } from "@/app/_constants/StackData";
+import UseStack from "@/app/_components/Stack/UseStack";
 
 interface StackListProps {
   stackList: string[];
@@ -10,19 +9,7 @@ function StackSection({ stackList }: StackListProps) {
   return (
     <section className="mt-10">
       <p className="mb-4 text-lg font-bold">사용한 스킬</p>
-      <div className="rounded-xl border border-solid border-[#EBEBEB] bg-white p-3">
-        <ul className="flex flex-wrap gap-4">
-          {stackList.map(stack => {
-            const stackItem = FULL_STACK_DATA.find(item => item.name === stack);
-            return (
-              <li className="flex items-center gap-1" key={stackItem?.id}>
-                <Image src={stackItem?.image || ""} alt="기술 스택 이미지." width={20} />
-                <p className="text-xs">{stack}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <UseStack stackList={stackList} />
     </section>
   );
 }
