0a6c9849a15bdeb86ca4a26c6735595ca508f700
JeongSabibi <jsbb528@naver.com>
2024-06-02 00:34:48 +0900

    💄 Design: 이미지 섹션 분리 및 UI 수정
    Design: 이미지 섹션 분리 및 UI 수정

--- app/addproject/page.tsx
+++ app/addproject/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import InputSection from "./_components/Section/InputSection";
 import DropDownSection from "./_components/Section/DropDownSection";
+import ImageSection from "./_components/Section/ImageSection";
 
 function page() {
   return (
@@ -21,28 +22,8 @@ function page() {
           inputWidth="w-full"
         />
         <InputSection title="본문" placeholder="본문을 작성해 주세요" textArea />
-        {/* 이미지는 승훈님이 제작하신 input 컴포넌트 사용해서 변경 예정 */}
-        <h2 className="mb-4 mt-6 text-base font-bold text-[#4D5256]">이미지</h2>
-        <div className="flex gap-3">
-          <label>
-            <input type="radio" name="size" value="웹" />웹
-          </label>
-          <label>
-            <input type="radio" name="size" value="모바일" />
-            모바일
-          </label>
-        </div>
-        <div className="mb-4 mt-4">
-          <div className="border-black-700 h-64 w-[360px] rounded-xl border-4 border-solid bg-slate-100" />
-          <p className="mt-3 text-xs font-medium text-[#C4C4C4]">1440 X 1024 사이즈로 업로드</p>
-        </div>
-        <div className="mb-4 mt-4">
-          <div className="border-black-700 h-[203px] w-[93.75px] rounded-xl border-4 border-solid bg-slate-100" />
-          <p className="mt-3 text-xs font-medium text-[#C4C4C4]">375 X 812 사이즈로 업로드</p>
-        </div>
-        <button className="rounded-md border border-solid border-[#1852FD] bg-[#1852FD] px-4 py-2 text-white">
-          업로드
-        </button>
+        <ImageSection title="썸네일" />
+        <ImageSection title="이미지" />
         <InputSection title="서비스 링크" inputType="text" placeholder="URL" inputWidth="w-[420px]" />
         <DropDownSection
           title="추가 링크"
