c6a0da0ec315af740819f6fd9ce86b38b928a625
thgus5335 <thgus5335@naver.com>
2024-06-02 15:58:31 +0900

    :lipstick: Design: 별점 UI 구현
    Design: 별점 UI 구현

---
+++ app/project/[projectId]/_components/Section/RatingSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import emptyStarIcon from "@/public/icons/emptyStar.svg";
+
+const ratingCategory = [
+  { id: 1, name: "아이디어", rate: 4.1 },
+  { id: 2, name: "디자인", rate: 4.2 },
+  { id: 3, name: "기능", rate: 4.3 },
+  { id: 4, name: "완성도", rate: 4.4 },
+];
+
+function RatingSection() {
+  return (
+    <section className="flex">
+      <div className="flex flex-col items-center gap-2">
+        <Image src={emptyStarIcon} alt={"프로젝트 전체 별점."} width={120} />
+        <p className="text-4xl font-bold text-[#323232]">3.5</p>
+      </div>
+      <div className="ms-12 flex w-full flex-col gap-4">
+        {ratingCategory.map(category => (
+          <div className="flex justify-between" key={category.id}>
+            <p className="text-xl text-[#434343]">{category.name}</p>
+            <div className="flex h-9 w-72 items-center bg-[#D9D9D9]">
+              <p className="ms-9 text-xl font-bold">{category.rate}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default RatingSection;
