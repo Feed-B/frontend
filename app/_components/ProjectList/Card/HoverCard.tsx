4e43926972997016e9c25157877b19761eb6a034
codingaring <sksms2104@naver.com>
2024-06-03 17:00:35 +0900

    ♻️ Refactor: 컴파운드 패턴을 이용한 projectList 리팩토링

---
+++ app/_components/ProjectList/Card/HoverCard.tsx
@@ -0,0 +1,30 @@
+import WishCount from "../../WishCount/WishCount";
+import StackBadge from "./StackBadge";
+
+function HoverCard({
+  stackList,
+  wishCount,
+  isWishProject,
+}: {
+  stackList: string[];
+  wishCount: number;
+  isWishProject: boolean;
+}) {
+  return (
+    <div className="flex p-6">
+      <div className="z-20">
+        <div className="grid grid-cols-3 gap-2.5">
+          {stackList.map((stack, index) => (
+            <StackBadge key={index} stack={stack} />
+          ))}
+        </div>
+        <div className="absolute bottom-5 right-5 text-white">
+          <WishCount isFavorite={isWishProject} wishCount={wishCount} />
+        </div>
+      </div>
+      <div className="absolute inset-0 z-10 bg-gradient-to-t from-black to-[rgba(0,0,0,0)] opacity-90" />
+    </div>
+  );
+}
+
+export default HoverCard;
