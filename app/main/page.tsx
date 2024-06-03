fb724dcbdd6342378d44886bc0f820a7cb973d84
codingaring <sksms2104@naver.com>
2024-06-03 22:31:38 +0900

    🔥 Remove: 불필요한 prop 삭제 및 파일명 변경

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -7,7 +7,7 @@ function MainPage() {
   return (
     <main className="mx-auto mt-[100px] grid w-[1200px] grid-cols-[230px_minmax(960px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
       <SelectStack />
-      <ProjectList projectList={mockDataCardList.myProjectList.data} gridCount={3} />
+      <ProjectList projectList={mockDataCardList.myProjectList.data} />
     </main>
   );
 }
