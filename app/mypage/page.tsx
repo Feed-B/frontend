190f724a992e1ef148360876d54eb1ff76d0a1c0
codingaring <sksms2104@naver.com>
2024-06-03 17:00:35 +0900

    ♻️ Refactor: 컴파운드 패턴을 이용한 projectList 리팩토링

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,11 +1,11 @@
-import CardList from "../_components/CardList/CardList";
 import Profile from "./_components/Profile";
+import ProfileProjectList from "./_components/ProfileProjectList";
 
 function MyPage() {
   return (
     <>
       <Profile />
-      <CardList />
+      <ProfileProjectList />
     </>
   );
 }
