dc00073e1995d95262251b4609ef6478e7a15450
78-artilleryman <ywy040150@gmail.com>
2024-06-04 01:31:32 +0900

    💄 Design: 카드리스트 레이아웃 넓이 변경
    design: 카드리스트 레이아웃 넓이 변경

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -3,7 +3,7 @@ import SelectStack from "./_components/SelectStack/SelectStack";
 
 function MainPage() {
   return (
-    <main className="mx-auto mt-[100px] grid w-[1200px] grid-cols-[230px_minmax(960px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
+    <main className="mx-auto mt-[100px] grid w-[1200px] grid-cols-[230px_minmax(976px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
       <SelectStack />
       <section className="col-start-2 mt-[80px]">프로젝트 리스트</section>
     </main>
