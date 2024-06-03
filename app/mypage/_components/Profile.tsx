7648828254aa1a4091e9ddd6a82cd4ea9e86cf4a
codingaring <sksms2104@naver.com>
2024-06-04 02:33:22 +0900

    💄 Design: 프로필 수정 UI 구현

--- app/mypage/_components/Profile.tsx
+++ app/mypage/_components/Profile.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
 import profileMock from "@/public/images/mock_profileImage.jpg";
 import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
 import addImageIcon from "@/public/icons/addImage.svg";
+import useToggleHook from "@/app/_hooks/useToggleHook";
+import EditProfileButton from "./EditProfileButton";
 
 const mockData = {
   nickName: "하늘을 나는 개발자",
@@ -9,21 +12,45 @@ const mockData = {
   imageUrl: profileMock,
 };
 
+// input은 공통 컴포넌트로 대체 예정입니다.
 function Profile() {
+  const { isOpen, toggleState } = useToggleHook();
   return (
-    <form className="flex items-start justify-start gap-7 rounded-lg border border-solid border-[#d9d9d9] p-10">
+    <form className="relative flex items-start justify-start gap-8 rounded-lg border border-solid border-[#d9d9d9] p-8">
       <div className="relative">
         <ProfileImage imageUrl={mockData.imageUrl} />
-        <label htmlFor="profile-image">
-          <div className="absolute bottom-1 right-0 h-10 w-10 cursor-pointer">
-            <Image fill src={addImageIcon} alt="이미지 추가" />
-          </div>
-        </label>
-        <input type="file" id="profile-image" className="hidden" />
+        {isOpen && (
+          <>
+            <label htmlFor="profile-image">
+              <div className="absolute bottom-1 right-0 h-10 w-10 cursor-pointer">
+                <Image fill src={addImageIcon} alt="이미지 추가" />
+              </div>
+            </label>
+            <input type="file" id="profile-image" className="hidden" />
+          </>
+        )}
       </div>
-      <div className="flex flex-col gap-1.5 pt-4">
-        <div className="text-lg font-semibold">{mockData.nickName}</div>
-        <div>{mockData.introduction}</div>
+      {isOpen ? (
+        <div className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder={mockData.nickName}
+            className="w-[250px] rounded-md border border-solid border-[#ebebeb] p-2 text-sm"
+          />
+          <textarea
+            placeholder={mockData.introduction}
+            className="h-[80px] w-[604px] resize-none rounded-md border border-solid border-[#ebebeb] p-2 text-sm"
+          />
+        </div>
+      ) : (
+        <div className="mt-2.5 flex flex-col gap-4">
+          <div className="text-xl font-semibold">{mockData.nickName}</div>
+          <div className="text-base text-[#454545]">{mockData.introduction}</div>
+        </div>
+      )}
+
+      <div className="absolute bottom-8 right-8">
+        <EditProfileButton onClick={toggleState}>{isOpen ? "수정 완료" : "프로필 수정"}</EditProfileButton>
       </div>
     </form>
   );
