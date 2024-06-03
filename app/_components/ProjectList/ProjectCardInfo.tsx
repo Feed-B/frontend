6a9e455da91b5e82ca88aa56b0d7de775487de3e
codingaring <sksms2104@naver.com>
2024-06-03 18:37:44 +0900

    🎨 Design : ProjectList 컴포넌트 메인 페이지 반영 및 수정

--- app/_components/ProjectList/ProjectCardInfo.tsx
+++ app/_components/ProjectList/ProjectCardInfo.tsx
@@ -11,12 +11,10 @@ function ProjectCardInfo({ projectTitle, projectSubDescription, viewCount }: Pro
     <div className="relative">
       <div className="flex flex-col gap-1">
         <div className="flex items-center justify-between">
-          <h6 className="w-[16rem] overflow-hidden text-ellipsis whitespace-nowrap text-lg font-bold">
-            {projectTitle}
-          </h6>
+          <h6 className="overflow-hidden text-ellipsis whitespace-nowrap font-bold">{projectTitle}</h6>
           <ViewCount viewCount={viewCount} />
         </div>
-        <p className="w-[21rem] overflow-hidden text-ellipsis whitespace-nowrap">{projectSubDescription}</p>
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap">{projectSubDescription}</p>
       </div>
     </div>
   );
