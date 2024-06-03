db2cecf4feeda9fb561aaf7cf9f08236b263b6db
thgus5335 <thgus5335@naver.com>
2024-06-03 20:49:11 +0900

    :sparkles: Feat: 프로젝트 페이지 케밥 드롭다운 구현
    Feat: 프로젝트 페이지 케밥 드롭다운 구현

--- app/project/[projectId]/_components/Project/ProjectHeader.tsx
+++ app/project/[projectId]/_components/Project/ProjectHeader.tsx
@@ -1,18 +1,28 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import favoriteIcon from "@/public/icons/favorite.svg";
 import shareIcon from "@/public/icons/share.svg";
 import kebabIcon from "@/public/icons/kebab.svg";
+import useToggleHook from "@/app/_hooks/useToggleHook";
+import KebabDropDown from "./KebabDropDown";
+
+//임시 ID
+const projectId = 1;
 
 function ProjectHeader() {
+  const { isOpen, toggleState } = useToggleHook();
+
   return (
-    <header className="px-4 py-3">
+    <header className="relative px-4 py-3">
       <div className="flex justify-between gap-2">
         <h1 className="text-2xl font-bold text-[#454545]">프로젝트_제목</h1>
         <div className="flex gap-2">
           <Image src={favoriteIcon} alt="프로젝트 찜하기." width={24} height={32} priority />
           <Image src={shareIcon} alt="프로젝트 공유하기." width={24} height={32} priority />
-          <Image src={kebabIcon} alt="프로젝트 메뉴." width={24} height={32} priority />
+          <Image src={kebabIcon} alt="프로젝트 메뉴." width={24} height={32} priority onClick={toggleState} />
+          {isOpen && <KebabDropDown projectId={projectId} />}
         </div>
       </div>
       <div className="flex w-full items-center gap-5">
