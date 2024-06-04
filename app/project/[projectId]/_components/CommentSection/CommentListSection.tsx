2473cbb4883f57ba2b812c9a01ea2848e0d8fae8
thgus5335 <thgus5335@naver.com>
2024-06-04 13:34:03 +0900

    :lipstick: Design: 댓글 리스트 UI 구현
    Design: 댓글 리스트 UI 구현

--- app/project/[projectId]/_components/CommentSection/CommentListSection.tsx
+++ app/project/[projectId]/_components/CommentSection/CommentListSection.tsx
@@ -1,86 +1,76 @@
 import React from "react";
-import CommentProfile from "../Comment/CommentProfile";
-import CommentCount from "../Comment/CommentCount";
-import TotalStar from "../Comment/TotalStar";
 import Pagination from "../Comment/Pagination";
+import CommentCard from "../Comment/CommentCard";
+
+//mock data
+const CommentList = [
+  {
+    id: 1,
+    name: "하잉",
+    comment:
+      "안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다. 안녕하세요 댓글입니다.",
+    rating: [
+      {
+        idea: 4.5,
+        design: 3.5,
+        feature: 3,
+        perfection: 5,
+      },
+    ],
+    total: 3.5,
+  },
+  {
+    id: 2,
+    name: "하잉2",
+    comment: "안녕하세요 댓글입니다.",
+    rating: [
+      {
+        idea: 4.5,
+        design: 3.5,
+        feature: 3,
+        perfection: 5,
+      },
+    ],
+    total: 3.5,
+  },
+  {
+    id: 3,
+    name: "하잉2",
+    comment: "안녕하세요 댓글입니다.",
+    rating: [
+      {
+        idea: 4.5,
+        design: 3.5,
+        feature: 3,
+        perfection: 5,
+      },
+    ],
+    total: 3.5,
+  },
+  {
+    id: 4,
+    name: "하잉2",
+    comment: "안녕하세요 댓글입니다.",
+    rating: [
+      {
+        idea: 4.5,
+        design: 3.5,
+        feature: 3,
+        perfection: 5,
+      },
+    ],
+    total: 3.5,
+  },
+];
 
 function CommentListSection() {
   return (
     <section className="mt-10">
       <h3 className="mb-4 text-lg font-bold">댓글</h3>
-      {/* cardList */}
       <div className="grid grid-cols-4 gap-x-6 gap-y-3">
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
-
-        <div className="flex flex-col gap-4 rounded-xl bg-[#F8FAFB] p-4">
-          <div className="flex justify-between">
-            <CommentProfile />
-            <CommentCount />
-          </div>
-          <p className="text-sm text-[#1C1C1C]">새벽에 혼자 있어서 넘 심심합니도. 새벽반 함께 하실 분 구합니도.(1/5)</p>
-          <TotalStar />
-        </div>
+        {CommentList.map(comment => (
+          <CommentCard key={comment.id} comment={comment} />
+        ))}
       </div>
       <Pagination />
     </section>
