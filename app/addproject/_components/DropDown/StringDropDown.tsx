352e5f8dde7afc73916bd45eea984eb39b9d5d15
JeongSabibi <jsbb528@naver.com>
2024-06-05 00:55:51 +0900

    ✨ Feat: 드롭다운 ref로 지정 후 hook 사용
    Feat: 드롭다운 ref로 지정 후 hook 사용

--- app/addproject/_components/DropDown/StringDropDown.tsx
+++ app/addproject/_components/DropDown/StringDropDown.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 interface Props {
-  handleItemClick: (value: string) => void;
   data: Record<string, string>;
+  dropdownRef: React.RefObject<HTMLDivElement>;
+  handleItemClick: (value: string) => void;
 }
 
-function StringDropDown({ handleItemClick, data }: Props) {
+function StringDropDown({ data, dropdownRef, handleItemClick }: Props) {
   return (
-    <>
+    <div ref={dropdownRef}>
       {Object.entries(data).map(([index, value]) => (
         <button type="button" key={index} className="block cursor-pointer p-2" onClick={() => handleItemClick(value)}>
           {value}
         </button>
       ))}
-    </>
+    </div>
   );
 }
 
