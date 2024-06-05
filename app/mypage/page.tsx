29cdc68108157288430d7447d55377d5454e2dab
codingaring <sksms2104@naver.com>
2024-06-05 17:19:09 +0900

    🎨 Refactor: layout 및 /mypage 수정

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,10 +3,10 @@ import ProfileProjectList from "./_components/ProfileProjectList";
 
 function MyPage() {
   return (
-    <>
+    <main className="mb-20 ml-[50%] mt-10 flex w-[90rem] max-w-[976px] -translate-x-1/2 flex-col gap-y-16">
       <Profile />
       <ProfileProjectList />
-    </>
+    </main>
   );
 }
 
