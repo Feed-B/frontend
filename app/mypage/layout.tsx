fe239e01869a4c3eddfa0df97ba23f5ac722acd9
codingaring <sksms2104@naver.com>
2024-06-05 17:19:09 +0900

    🎨 Refactor: layout 및 /mypage 수정

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -4,12 +4,12 @@ import Header from "../_components/Header/Header";
 
 function OnlyHeader({ children }: PropsWithChildren) {
   return (
-    <>
-      <Header />
-      <div className="mb-20 ml-[50%] mt-10 flex w-[90rem] max-w-[976px] -translate-x-1/2 flex-col gap-y-16">
+    <html lang="ko">
+      <body>
+        <Header />
         {children}
-      </div>
-    </>
+      </body>
+    </html>
   );
 }
 
