d1d57bebc61cab1f2765b94f2909a288808718bd
78-artilleryman <ywy040150@gmail.com>
2024-06-05 22:51:31 +0900

    🎨 Style: 카드리스트 레이아웃 설정
    style: 카드리스트 레이아웃 설정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -5,10 +5,11 @@ import SelectStack from "./_components/SelectStack/SelectStack";
 
 function MainPage() {
   return (
-
     <main className="mx-auto mt-[100px] grid w-[1200px] grid-cols-[230px_minmax(976px,_1fr)] grid-rows-[100px_minmax(800px,_1fr)]">
       <SelectStack />
-      <ProjectList projectList={mockDataCardList.myProjectList.data} />
+      <section className="col-start-2 mt-[40px]">
+        <ProjectList projectList={mockDataCardList.myProjectList.data} />
+      </section>
     </main>
   );
 }
