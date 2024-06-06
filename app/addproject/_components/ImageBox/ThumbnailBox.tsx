c0c906b5cd3d81c13dc320fd5af677ef14e6f2e4
JeongSabibi <jsbb528@naver.com>
2024-06-06 16:40:04 +0900

    🎨 Style: 공통 컴포넌트로 구현된 Button으로 수정
    Style: 공통 컴포넌트로 구현된 Button으로 수정

--- app/addproject/_components/ImageBox/ThumbnailBox.tsx
+++ app/addproject/_components/ImageBox/ThumbnailBox.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@/app/_components/Button/Button";
 
 const ThumbnailSize = "232 X 232 사이즈로 업로드";
 
@@ -9,11 +10,9 @@ function ThumbnailBox() {
         <div className="h-[232px] w-[232px] rounded-xl border-2 border-dashed border-[#D4D4D4] bg-[#F8FAFB]" />
         <p className="mt-3 text-xs font-medium text-[#C4C4C4]">{ThumbnailSize}</p>
       </div>
-      <button
-        type="button"
-        className="rounded-md border border-solid border-[#1852FD] bg-[#1852FD] px-4 py-2 text-white">
-        업로드
-      </button>
+      <Button buttonSize="small" bgColor="mainBlue">
+        <p>업로드</p>
+      </Button>
     </>
   );
 }
