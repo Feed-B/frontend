cb66dc1a8960083bb38bc27d0763a130b2add719
78-artilleryman <ywy040150@gmail.com>
2024-06-08 00:06:27 +0900

    🎨 Style: profileImage 컴포넌트 수정 및 적용
    style: profileImage 컴포넌트 수정 및 적용

--- app/_components/Header/HeaderDropDownBox.tsx
+++ app/_components/Header/HeaderDropDownBox.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import SmallArrowIcon from "@/public/icons/smallArrow.svg";
 import SmallTopArrowIcon from "@/public/icons/smallTopArrow.svg";
 import useToggleHook from "@/app/_hooks/useToggleHook";
-import Profile from "../Profile/Profile";
 import DropDown from "../DropDown/DropDown";
+import ProfileImage from "../ProfileImage/ProfileImage";
 
 function HeaderDropDownBox() {
   const { isOpen, toggleState } = useToggleHook();
@@ -14,7 +14,7 @@ function HeaderDropDownBox() {
   return (
     <>
       <div className="relative flex cursor-pointer items-center gap-2" onClick={toggleState}>
-        <Profile />
+        <ProfileImage imageUrl={""} className="h-9 w-9" />
         <button type="button" className="h-5 w-5">
           {isOpen ? (
             <Image src={SmallTopArrowIcon} alt="open_dropbox" width={20} height={20} priority />
--- app/project/[projectId]/_components/CommnetModal/ReflyCommentItem.tsx
+++ app/project/[projectId]/_components/CommnetModal/ReflyCommentItem.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Profile from "@/app/_components/Profile/Profile";
+import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
 
 function ReflyCommentItem() {
   return (
     <div className="mt-1 flex gap-1 p-2">
-      <Profile />
+      <ProfileImage imageUrl={""} className="h-10 w-10" />
       <div>
         <p className="text-sm font-normal text-[#1C1C1C]">김한주</p>
         <p className="text-sm font-normal text-[#454545]">모달 만들어버리기</p>
