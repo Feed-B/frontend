62c01f5e9f20cb0b5fcdaf135dca105cc2415b2d
thgus5335 <thgus5335@naver.com>
2024-06-07 15:39:24 +0900

    :bug: Fix: Comment 관련 color 코드 수정
    Fix: Comment 관련 color 코드 수정

--- app/project/[projectId]/_components/Comment/Pagination.tsx
+++ app/project/[projectId]/_components/Comment/Pagination.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Pagination() {
   return (
-    <div className="mt-8 flex justify-center gap-5 p-2">
+    <div className="mt-8 flex justify-center gap-5 p-2 text-gray-900">
       <p>1</p>
       <p>2</p>
       <p>3</p>
--- app/project/[projectId]/_components/Comment/TotalStar.tsx
+++ app/project/[projectId]/_components/Comment/TotalStar.tsx
@@ -5,10 +5,10 @@ import starIcon from "@/public/icons/star2.svg";
 function TotalStar() {
   return (
     <div className="h-[60px]">
-      <p className="mb-1 text-base font-semibold text-[#1C1C1C]">총점</p>
+      <p className="mb-1 text-base font-semibold text-gray-900">총점</p>
       <div className="flex items-center gap-2">
         <Image src={starIcon} alt="총별점" width={24} />
-        <p className="text-black text-xl font-bold">3.5</p>
+        <p className="text-xl font-bold text-black">3.5</p>
       </div>
     </div>
   );
