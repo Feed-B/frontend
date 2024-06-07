83cf8a8dcfcf39661b54803dcfaf454e3375c6f9
codingaring <sksms2104@naver.com>
2024-06-07 17:24:37 +0900

    🎨 Style: WishButtonAndCount colorMode prop 추가

--- app/_components/WishButtonAndCount/WishButtonAndCount.tsx
+++ app/_components/WishButtonAndCount/WishButtonAndCount.tsx
@@ -3,13 +3,15 @@ import { MouseEvent, useState } from "react";
 import Image from "next/image";
 import emptyHeartIcon from "@/public/icons/emptyHeart.svg";
 import fullHeartIcon from "@/public/icons/fullHeart.svg";
+import emptyBlackHeartIcon from "@/public/icons/emptyBlackHeart.svg";
 
 interface WishButtonAndCountProps {
   isFavorite: boolean;
   wishCount: number;
+  colorMode?: "bright" | "dark";
 }
 
-function WishButtonAndCount({ isFavorite = false, wishCount }: WishButtonAndCountProps) {
+function WishButtonAndCount({ isFavorite = false, wishCount, colorMode = "bright" }: WishButtonAndCountProps) {
   const [favoriteState, setFavoriteState] = useState({
     isFavorite: isFavorite,
     wishCountState: wishCount,
@@ -26,9 +28,14 @@ function WishButtonAndCount({ isFavorite = false, wishCount }: WishButtonAndCoun
   return (
     <div className="flex items-center justify-center gap-0.5">
       <button type="button" className="relative h-4 w-4" onClick={handleFavorite}>
-        <Image fill src={favoriteState.isFavorite ? fullHeartIcon : emptyHeartIcon} alt="favorite-project" />
+        <Image
+          fill
+          src={favoriteState.isFavorite ? fullHeartIcon : colorMode === "bright" ? emptyHeartIcon : emptyBlackHeartIcon}
+          alt="favorite-project"
+        />
       </button>
-      <p className={`min-w-5 text-center text-sm ${favoriteState.isFavorite ? "text-[#FF0000]" : "text-white"} `}>
+      <p
+        className={`min-w-5 text-center text-sm ${favoriteState.isFavorite ? "text-[#FF0000]" : colorMode === "bright" ? "text-white" : "text-gray-900"}`}>
         {favoriteState.wishCountState}
       </p>
     </div>
