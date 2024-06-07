5d5769762083bfe30f3f629aec8c90176729f822
thgus5335 <thgus5335@naver.com>
2024-06-07 15:39:24 +0900

    :bug: Fix: Comment 관련 color 코드 수정
    Fix: Comment 관련 color 코드 수정

--- app/project/[projectId]/_components/CommentSection/WriteCommentSection.tsx
+++ app/project/[projectId]/_components/CommentSection/WriteCommentSection.tsx
@@ -13,11 +13,11 @@ const ratingCategory = [
 function WriteCommentSection() {
   return (
     <section className="flex flex-col gap-4 p-6">
-      <p className="text-base font-bold text-[#1C1C1C]">프로젝트를 평가해주세요</p>
+      <p className="text-base font-bold text-gray-900">프로젝트를 평가해주세요</p>
       <div className="flex w-full  justify-between">
         {ratingCategory.map(category => (
           <div className="flex flex-col gap-1.5" key={category.id}>
-            <p className="text-sm text-[#1C1C1C]">{category.name}</p>
+            <p className="text-sm text-gray-900">{category.name}</p>
             <div className="flex items-center gap-0.5">
               {/* 추후 기능 추가시 수정 예정 */}
               <Image src={fullStarIcon} alt="프로젝트 평가 별점." width={32} />
@@ -30,14 +30,14 @@ function WriteCommentSection() {
           </div>
         ))}
       </div>
-      <div className="flex h-32 gap-1 rounded-lg bg-[#F8F8F8] p-3">
+      <div className="flex h-32 gap-1 rounded-lg bg-gray-100 p-3 text-gray-500">
         <textarea
-          className="h-full w-full resize-none bg-transparent outline-none"
+          className="bg-transparent h-full w-full resize-none outline-none"
           placeholder="주제와 무관한 댓글은 삭제될 수 있습니다."
         />
         <div className="flex flex-col justify-between">
-          <p className="text-end text-sm text-[#404040]">0/150</p>
-          <button className="whitespace-nowrap rounded-lg bg-[#454545] px-4 py-3 text-[#FFFFFF]">댓글 등록</button>
+          <p className="text-end text-sm text-gray-600">0/150</p>
+          <button className="whitespace-nowrap rounded-lg bg-gray-400 px-4 py-3 text-white">댓글 등록</button>
         </div>
       </div>
     </section>
