b578b7d99c9b630bb90b79d6d0192bc6982f3fa9
78-artilleryman <ywy040150@gmail.com>
2024-06-07 18:01:42 +0900

    💄 Design: 공통 모달 디자인 변경 및 모달 연결
    design: 공통 모달 디자인 변경 및 모달 연결

--- app/_components/Modal/Modal.tsx
+++ app/_components/Modal/Modal.tsx
@@ -12,20 +12,19 @@ interface ModalProps extends PropsWithChildren {
 
 function Modal({ children, openModal, handleModalClose, className }: ModalProps) {
   const modalClass = twMerge(
-    "flex flex-col fixed top-1/2 left-1/2  bg-white rounded-lg zIndex-modal transform -translate-x-1/2 -translate-y-1/2 px-28 py-32  mobile:px-20 mobile:py-28",
+    "fixed top-1/2 left-1/2 bg-white rounded-lg z-50 transform -translate-x-1/2 -translate-y-1/2 px-28 py-32 mobile:px-20 mobile:py-28",
     className
   );
 
   const open = "block";
 
-  const handleOverlayClick = () => {
-    handleModalClose();
-  };
-
   return (
     <ModalPortal>
-      <div className="fixed left-0 top-0 h-full w-full bg-black-overlay" onClick={handleOverlayClick} />
-      <div className={`${modalClass} ${openModal ? open : ""}`}>{children}</div>
+      <div className="fixed left-0 top-0 z-50 h-full w-full bg-black-overlay" onClick={handleModalClose}>
+        <div onClick={e => e.stopPropagation()} className={`${modalClass} ${openModal ? open : ""}`}>
+          {children}
+        </div>
+      </div>
     </ModalPortal>
   );
 }
