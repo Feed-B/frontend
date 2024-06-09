10ae907d0dd61c8b497c21d4dcdc7c498a0f82a1
JeongSabibi <jsbb528@naver.com>
2024-06-09 23:33:29 +0900

    💄 Design: 프로젝트 업로드 순서 변경
    Design: 프로젝트 업로드 순서 변경

--- app/addproject/page.tsx
+++ app/addproject/page.tsx
@@ -11,6 +11,7 @@ function page() {
       <div className="w-[960px]">
         <h1 className="mb-4 mt-6 text-[28px] font-bold text-gray-900">프로젝트 업로드</h1>
         <hr />
+        <ImageSection title="썸네일" />
         <InputSection
           title="프로젝트 이름"
           type="text"
@@ -23,19 +24,12 @@ function page() {
           type="text"
           placeholder="프로젝트에 대한 소개를 작성 주세요"
           inputWidth="w-full"
-          name="projectDescription"
+          name="projectIntroduction"
         />
-        <InputSection title="본문" placeholder="본문을 작성해 주세요" textArea />
+        <InputSection title="본문" placeholder="본문을 작성해 주세요" name="projectDescription" textArea />
         <InputSection title="프로젝트 링크" type="text" placeholder="URL" inputWidth="w-[420px]" name="serviceLink" />
-        <DropDownSection
-          title="추가 링크"
-          type="text"
-          placeholder="URL"
-          name="addLink"
-          inputWidth="w-96"
-          dropDownType="tool"
-          dropDownWidth="w-28"
-        />
+        <ImageSection title="이미지" />
+        <StackSection />
         <DropDownSection
           title="팀원"
           type="text"
@@ -45,9 +39,15 @@ function page() {
           dropDownType="job"
           dropDownWidth="w-44"
         />
-        <StackSection />
-        <ImageSection title="썸네일" />
-        <ImageSection title="이미지" />
+        <DropDownSection
+          title="추가 링크"
+          type="text"
+          placeholder="URL"
+          name="addLink"
+          inputWidth="w-96"
+          dropDownType="tool"
+          dropDownWidth="w-28"
+        />
         <div className="flex justify-center">
           <Button buttonSize="small" bgColor="mainBlue" className="mt-6">
             <p>등록하기</p>
