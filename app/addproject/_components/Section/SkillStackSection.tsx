367a7290f8d62f502ee8a7671c9fe0e495b973ee
JeongSabibi <jsbb528@naver.com>
2024-06-09 23:36:25 +0900

    ✨ Feat: 스택 삭제하는 기능 구현
    Feat: 스택 삭제하는 기능 구현

--- app/addproject/_components/Section/SkillStackSection.tsx
+++ app/addproject/_components/Section/SkillStackSection.tsx
@@ -1,84 +1,130 @@
 "use client";
 
-import React, { ChangeEventHandler, FocusEventHandler, MouseEventHandler, useState } from "react";
+import React, { ChangeEventHandler, FocusEventHandler, MouseEventHandler, useEffect, useState } from "react";
 import Image from "next/image";
 import searchIcon from "@/public/icons/search.svg";
 import { FULL_STACK_DATA } from "@/app/_constants/StackData";
+import crossLineIcon from "@/public/icons/crossLine.svg";
 
 function SkillStackSection() {
   const stackData = FULL_STACK_DATA;
   const [isHidden, setIsHidden] = useState(true);
   const [liOver, setLiOver] = useState(false);
   const [result, setResult] = useState<Set<string>>(new Set());
+  const [selectedStacks, setSelectedStacks] = useState<string[]>([]);
   const [search, setSearch] = useState("");
 
-  const onChange: ChangeEventHandler<HTMLInputElement> = e => {
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = e => {
     const { value } = e.currentTarget;
     setSearch(value);
   };
 
-  const onFocusIn: FocusEventHandler<HTMLInputElement> = () => {
+  const handleInputFocusIn: FocusEventHandler<HTMLInputElement> = () => {
     setIsHidden(false);
   };
-  const onFocusOut: FocusEventHandler<HTMLInputElement> = () => {
+  const handleInputFocusOut: FocusEventHandler<HTMLInputElement> = () => {
     if (liOver) return;
     setIsHidden(true);
   };
 
-  const onMouseOver: MouseEventHandler<HTMLLIElement> = () => {
+  const handleMouseOver: MouseEventHandler<HTMLLIElement> = () => {
     setLiOver(true);
   };
-  const onMouseLeave: MouseEventHandler<HTMLLIElement> = () => {
+  const handleMouseLeave: MouseEventHandler<HTMLLIElement> = () => {
     setLiOver(false);
   };
 
-  const onAddResultClick = (stack: string) => {
+  const handleAddStack = (stack: string) => {
     setResult(prev => {
       const newSelectedStacks = new Set(prev);
       if (newSelectedStacks.has(stack)) {
-        newSelectedStacks.delete(stack);
+        return newSelectedStacks;
       } else {
         newSelectedStacks.add(stack);
       }
       return newSelectedStacks;
     });
+    setSearch("");
     setIsHidden(true);
   };
 
+  const handleDeleteStack = (stack: string) => {
+    setResult(prev => {
+      const newSelectedStacks = new Set(prev);
+      newSelectedStacks.delete(stack);
+      return newSelectedStacks;
+    });
+  };
+
+  useEffect(() => {
+    setSelectedStacks(Array.from(result));
+  }, [result]);
+
   return (
-    <section className="flex flex-col">
-      <label htmlFor="search" className="mb-4 mt-6 flex text-base font-bold text-gray-900">
+    <section className="flex flex-col gap-4">
+      <label htmlFor="search" className="mt-6 flex text-base font-bold text-gray-900">
         기술스택 *
       </label>
-      <div>{result}</div>
-      <div className="flex flex-row items-center gap-3 rounded-lg border border-solid border-gray-400 p-3">
-        <Image src={searchIcon} alt="검색 아이콘" width={16} priority />
-        <input
-          className="w-full text-sm font-normal outline-none"
-          name="search"
-          id="search"
-          placeholder="활용한 스킬을 검색해 주세요"
-          onChange={onChange}
-          value={search}
-          onFocus={onFocusIn}
-          onBlur={onFocusOut}
-        />
+      <div className="relative">
+        <div className="mb-1 flex flex-row items-center gap-3 rounded-lg border border-solid border-gray-400 p-3">
+          <Image src={searchIcon} alt="검색 아이콘" width={16} priority />
+          <input
+            className="w-full text-sm font-normal outline-none"
+            name="search"
+            id="search"
+            placeholder="활용한 스킬을 검색해 주세요"
+            onChange={handleInputChange}
+            value={search}
+            onFocus={handleInputFocusIn}
+            onBlur={handleInputFocusOut}
+            autoComplete="off"
+          />
+          <Image
+            src={crossLineIcon}
+            width={20}
+            alt="기술스택 삭제"
+            className="cursor-pointer"
+            onClick={() => setSearch("")}
+            priority
+          />
+        </div>
+        {!isHidden ? (
+          <ul className="absolute flex h-32 w-full flex-col items-start gap-1 overflow-y-auto rounded border border-solid border-gray-200 bg-white">
+            {stackData.map(data => (
+              <li
+                key={data.id}
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => handleAddStack(data.name)}
+                hidden={!data.name.includes(search)}
+                className="w-full cursor-pointer p-2 text-base font-medium text-gray-900 hover:bg-gray-100">
+                {data.name}
+              </li>
+            ))}
+          </ul>
+        ) : null}
       </div>
-      {!isHidden ? (
-        <ul className="flex flex-col items-start gap-1 rounded border border-solid border-gray-200">
-          {stackData.map(data => (
-            <li
-              key={data.id}
-              onMouseOver={onMouseOver}
-              onMouseLeave={onMouseLeave}
-              onClick={() => onAddResultClick(data.name)}
-              hidden={!data.name.includes(search)}
-              className="cursor-pointer p-2 text-base font-medium text-gray-900">
-              {data.name}
-            </li>
-          ))}
-        </ul>
-      ) : null}
+      <div className="flex min-h-9 flex-wrap gap-4">
+        {selectedStacks.map(stack => {
+          const stackItem = stackData.find(item => item.name === stack);
+          return (
+            <div
+              key={stackItem?.id}
+              className="flex items-center rounded-[44px] bg-gray-100 p-2 text-sm font-normal text-gray-900">
+              <Image src={stackItem?.image || ""} alt="기술 스택 이미지" width={20} className="mr-1" />
+              <p className="mr-1.5">{stack}</p>
+              <Image
+                src={crossLineIcon}
+                width={20}
+                alt="기술스택 삭제"
+                className="cursor-pointer"
+                onClick={() => handleDeleteStack(stack)}
+                priority
+              />
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
