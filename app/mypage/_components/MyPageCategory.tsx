dbf8edcf143d5a0b5e6ae64451790eaf185b8058
codingaring <sksms2104@naver.com>
2024-06-10 16:17:49 +0900

    ♻️ Refactor: /mypage 카테고리 버튼 리팩토링

--- app/mypage/_components/MyPageCategory.tsx
+++ app/mypage/_components/MyPageCategory.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { MouseEvent } from "react";
 import { myProjectList, wishProjectList } from "@/app/_components/ProjectList/mockDataCardList";
 import selectProfileIcon from "@/public/icons/blackProfile.svg";
@@ -18,41 +17,22 @@ function MyPageCategory({ selectCategory, handleSelectCategory }: MyPageCategory
   return (
     <div className="flex flex-col gap-2">
       <div className="text-lg font-bold text-gray-900">{MY_PAGE_TEXT.MY_PAGE}</div>
-      <ProjectCategoryButton onClick={handleSelectCategory} id={"myProject"} isSelect={selectCategory === "myProject"}>
-        <div className="flex items-center gap-2.5">
-          <Image
-            width={24}
-            height={24}
-            src={selectCategory === "myProject" ? selectProfileIcon : defaultProfileIcon}
-            alt="내 프로젝트 보기"
-            priority
-          />
-          <div
-            className={`text-sm font-semibold ${selectCategory === "myProject" ? defaultHeartIcon : selectHeartIcon}`}>
-            {MY_PAGE_TEXT.MY_PROJECT}
-          </div>
-        </div>
-        <div>{`(${myProjectList.count})`}</div>
-      </ProjectCategoryButton>
+      <ProjectCategoryButton
+        onClick={handleSelectCategory}
+        id={"myProject"}
+        isSelect={selectCategory === "myProject"}
+        icon={selectCategory === "myProject" ? selectProfileIcon : defaultProfileIcon}
+        text="MY_PROJECT"
+        count={`(${myProjectList.count})`}
+      />
       <ProjectCategoryButton
         onClick={handleSelectCategory}
         id={"wishProject"}
-        isSelect={selectCategory === "wishProject"}>
-        <div className="flex items-center gap-2.5">
-          <Image
-            width={24}
-            height={24}
-            src={selectCategory === "wishProject" ? selectHeartIcon : defaultHeartIcon}
-            alt="찜한 프로젝트 보기"
-            priority
-          />
-          <div
-            className={`text-sm font-semibold ${selectCategory === "wishProject" ? defaultHeartIcon : selectHeartIcon}`}>
-            {MY_PAGE_TEXT.WISH_PROJECT}
-          </div>
-        </div>
-        <div>{`(${wishProjectList.count})`}</div>
-      </ProjectCategoryButton>
+        isSelect={selectCategory === "wishProject"}
+        icon={selectCategory === "wishProject" ? selectHeartIcon : defaultHeartIcon}
+        text="WISH_PROJECT"
+        count={`(${wishProjectList.count})`}
+      />
     </div>
   );
 }
