0ab93dd72a04334cea28fd0dfca7f4c719cd939b
codingaring <sksms2104@naver.com>
2024-06-10 18:57:47 +0900

    🎨 Style: 프로필 수정 모달 UI 구현

--- app/mypage/_components/EditProfileModal.tsx
+++ app/mypage/_components/EditProfileModal.tsx
@@ -1,54 +1,60 @@
+import Image from "next/image";
 import Button from "@/app/_components/Button/Button";
 import Modal from "@/app/_components/Modal/Modal";
 import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
 import profileMock from "@/public/images/mock_profileImage.jpg";
+import closeButton from "@/public/icons/crossLine.svg";
+import Input from "@/app/_components/Input/Input";
+import { ProfileDataType } from "./Profile";
 
 interface EditProfileModalProps {
   openModal: boolean;
   handleModalClose: () => void;
+  profileData: ProfileDataType;
 }
 
-function EditProfileModal({ openModal, handleModalClose }: EditProfileModalProps) {
+function EditProfileModal({ openModal, handleModalClose, profileData }: EditProfileModalProps) {
   return (
-    <Modal openModal={openModal} handleModalClose={handleModalClose}>
+    <Modal
+      openModal={openModal}
+      handleModalClose={handleModalClose}
+      className="flex flex-col items-center gap-8 px-10 py-8">
+      <button type="button" className="relative h-6 w-6 self-end rounded-full active:bg-gray-200">
+        <Image fill src={closeButton} alt="프로필 수정 닫기" />
+      </button>
       <div className="flex flex-col items-center justify-center gap-8">
         <div className="flex gap-24">
           <div className="flex flex-col gap-5">
             <ProfileImage imageUrl={profileMock} className="h-[124px] w-[124px]" />
-            <label
-              htmlFor="profile-image"
-              className="rounded-md py-3 text-center text-sm text-blue-500 active:bg-gray-200">
-              이미지 수정
+            <label htmlFor="profile-image">
+              <Button type="button" bgColor="stroke" buttonSize="normal">
+                이미지 수정
+              </Button>
             </label>
             <input type="file" id="profile-image" className="hidden" />
           </div>
-          <div className="flex flex-col gap-2.5">
-            <label htmlFor="nickName" className="flex flex-col gap-2.5">
-              <p>닉네임</p>
-              <input id="nickName" type="text" className="rounded-md border border-solid border-gray-300" />
-            </label>
-            <label htmlFor="job" className="flex flex-col gap-2.5">
-              <p>직무</p>
-              <input id="job" type="text" className="rounded-md border border-solid border-gray-300" />
-            </label>
+          <div className="flex flex-col gap-4">
+            <Input inputSize="normal" name="nickName" type="text" placeholder={profileData.nickName} title="닉네임" />
+            <p className="text-base font-bold text-gray-900">직무</p>
+            {/** 프로젝트 등록할 때 drop down이랑 동일해보여서, 그걸 가져다가 쓸 것 같습니다. */}
             <label htmlFor="introduction" className="flex flex-col gap-2.5">
-              <p>소개</p>
+              <p className="text-base font-bold text-gray-900">소개</p>
               <textarea
                 id="introduction"
-                className="h-[384px] w-[384px] resize-none rounded-md border border-solid border-gray-300 px-2 py-3"
+                className="h-[384px] w-[384px] resize-none rounded-lg border border-gray-200 px-2 py-3 text-gray-500 placeholder:text-sm"
                 placeholder={"자신을 표현할 간단한 소개를 적어주세요(최대 150자)"}
               />
             </label>
           </div>
         </div>
-        <div className="flex gap-4">
-          <Button type="button" buttonSize="normal" bgColor="white">
-            취소
-          </Button>
-          <Button type="button" buttonSize="normal" bgColor="mainBlue">
-            수정 완료
-          </Button>
-        </div>
+      </div>
+      <div className="flex gap-4">
+        <Button type="button" buttonSize="normal" bgColor="white">
+          취소
+        </Button>
+        <Button type="button" buttonSize="normal" bgColor="mainBlue">
+          수정 완료
+        </Button>
       </div>
     </Modal>
   );
--- app/mypage/_components/Profile.tsx
+++ app/mypage/_components/Profile.tsx
@@ -1,8 +1,7 @@
 "use client";
-import Image from "next/image";
+import { StaticImageData } from "next/image";
 import profileMock from "@/public/images/mock_profileImage.jpg";
 import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
-import addImageIcon from "@/public/icons/addImage.svg";
 import useToggleHook from "@/app/_hooks/useToggleHook";
 import EditProfileButton from "./EditProfileButton";
 import { MY_PAGE_TEXT } from "./constant";
@@ -15,47 +14,28 @@ const mockData = {
   imageUrl: profileMock,
 };
 
+export interface ProfileDataType {
+  userId: number;
+  nickName: string;
+  introduction: string;
+  imageUrl: StaticImageData | string;
+}
+
 function Profile() {
   const { isOpen, toggleState } = useToggleHook();
   return (
     <form className="relative flex items-start justify-start gap-8 rounded-lg border border-solid border-gray-200 p-8">
       <div className="relative">
         <ProfileImage imageUrl={mockData.imageUrl} className="h-[120px] w-[120px]" />
-        {isOpen && (
-          <>
-            <EditProfileModal openModal={!isOpen} handleModalClose={toggleState} />
-            <label htmlFor="profile-image">
-              <div className="absolute bottom-1 right-0 h-10 w-10 cursor-pointer">
-                <Image fill src={addImageIcon} alt="이미지 추가" />
-              </div>
-            </label>
-            <input type="file" id="profile-image" className="hidden" />
-          </>
-        )}
+        {isOpen && <EditProfileModal profileData={mockData} openModal={!isOpen} handleModalClose={toggleState} />}
       </div>
-      {isOpen ? (
-        <div className="flex flex-col gap-4">
-          <input
-            type="text"
-            placeholder={mockData.nickName}
-            className="w-[250px] rounded-md border border-solid border-gray-200 p-2 text-sm"
-          />
-          <textarea
-            placeholder={mockData.introduction}
-            className="h-[80px] w-[604px] resize-none rounded-md border border-solid border-gray-200 p-2 text-sm"
-          />
-        </div>
-      ) : (
-        <div className="mt-2.5 flex flex-col gap-4">
-          <div className="text-xl font-semibold text-gray-900">{mockData.nickName}</div>
-          <div className="text-base text-gray-700">{mockData.introduction}</div>
-        </div>
-      )}
+      <div className="mt-2.5 flex flex-col gap-4">
+        <div className="text-xl font-semibold text-gray-900">{mockData.nickName}</div>
+        <div className="text-base text-gray-700">{mockData.introduction}</div>
+      </div>
 
       <div className="absolute bottom-8 right-8">
-        <EditProfileButton onClick={toggleState}>
-          {isOpen ? MY_PAGE_TEXT.COMPLETE_EDIT_PROFILE : MY_PAGE_TEXT.EDIT_PROFILE}
-        </EditProfileButton>
+        <EditProfileButton onClick={toggleState}>{MY_PAGE_TEXT.EDIT_PROFILE}</EditProfileButton>
       </div>
     </form>
   );
