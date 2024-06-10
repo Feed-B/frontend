e4eadfe244dabf41d6a2dbf17ec2db904ea56399
JeongSabibi <jsbb528@naver.com>
2024-06-10 12:20:28 +0900

    ♻️ Refactor: Image Section 분리 대신 page 파일에서 각 썸네일, 이미지 컴포넌트 바로 호출
    Refactor: Image Section 분리 대신 page 파일에서 각 썸네일, 이미지 컴포넌트 바로 호출

--- app/addproject/_components/ImageBox/ThumbnailBox.tsx
+++ app/addproject/_components/ImageBox/ThumbnailBox.tsx
@@ -3,15 +3,11 @@ import Button from "@/app/_components/Button/Button";
 
 const ThumbnailSize = "(232 x 232px)";
 
-interface ThumbnailBoxProps {
-  title: string;
-}
-
-function ThumbnailBox({ title }: ThumbnailBoxProps) {
+function ThumbnailBox() {
   return (
     <section className="flex flex-col gap-4">
       <div className="flex w-[232px] items-center justify-between">
-        <h2 className="text-base font-bold text-gray-900">{title} *</h2>
+        <h2 className="text-base font-bold text-gray-900">썸네일 *</h2>
         <span className="text-sm font-normal text-gray-500">{ThumbnailSize}</span>
       </div>
       <div className="flex h-[232px] w-[232px] items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-100">
