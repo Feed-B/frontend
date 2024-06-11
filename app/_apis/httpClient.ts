337e1106d39667163bc4a7e6063722c47c785769
thgus5335 <thgus5335@naver.com>
2024-06-12 00:45:00 +0900

    :sparkles: Feat: httpClient 구현
    Feat: httpClient 구현

---
+++ app/_apis/httpClient.ts
@@ -0,0 +1,51 @@
+function httpClient(BASE_URL: string) {
+  async function get<R>(url: string) {
+    const response = await fetch(BASE_URL + url);
+    const result: R = await response.json();
+    return result;
+  }
+
+  async function post<T, P>(url: string, data: P) {
+    const response = await fetch(BASE_URL + url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result: T = await response.json();
+    return result;
+  }
+
+  async function put<T, P>(url: string, data: P) {
+    const response = await fetch(BASE_URL + url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result: T = await response.json();
+    return result;
+  }
+
+  async function del<T>(url: string) {
+    const response = await fetch(BASE_URL + url, {
+      method: "DELETE",
+    });
+
+    const result: T = await response.json();
+    return result;
+  }
+
+  return {
+    get,
+    post,
+    put,
+    delete: del,
+  };
+}
+
+export default httpClient;
