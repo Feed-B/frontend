2b9636688e1a0a7709a0c8ccee5f2673a1b40e22
JeongSabibi <jsbb528@naver.com>
2024-06-11 16:54:07 +0900

    🎨 Style: 공통 컴포넌트 DropDown 사용
    Style: 공통 컴포넌트 DropDown 사용

--- app/addproject/_components/DropDown/DropDownBox.tsx
+++ app/addproject/_components/DropDown/DropDownBox.tsx
@@ -8,6 +8,7 @@ import useToggleHook from "@/app/_hooks/useToggleHook";
 import JOB_CATEGORIES from "@/app/_constants/JobCategoryData";
 import TOOL_DATA from "@/app/_constants/ToolData";
 import useOutsideClick from "@/app/_hooks/useOutsideClick";
+import DropDown from "@/app/_components/DropDown/DropDown";
 import DropDownList from "./DropDownList";
 
 interface DropDownProps {
@@ -15,7 +16,7 @@ interface DropDownProps {
   dropDownWidth?: string;
 }
 
-function DropDown({ dataType, dropDownWidth }: DropDownProps) {
+function DropDownBox({ dataType, dropDownWidth }: DropDownProps) {
   const dataMap: Record<string, Record<string, string>> = {
     job: JOB_CATEGORIES,
     tool: TOOL_DATA,
@@ -27,10 +28,10 @@ function DropDown({ dataType, dropDownWidth }: DropDownProps) {
 
   const [item, setItem] = useState(data && Object.values(data)[0]);
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const itemRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  useOutsideClick(dropdownRef, toggleState, buttonRef);
+  useOutsideClick(itemRef, toggleState, buttonRef);
 
   const handleItemClick = (value: string) => {
     setItem(value);
@@ -49,13 +50,12 @@ function DropDown({ dataType, dropDownWidth }: DropDownProps) {
         )}
       </button>
       {isOpen && (
-        <div
-          className={`absolute left-0 top-12 ${dropDownWidth} z-20 rounded-lg border border-solid border-gray-300 bg-white px-4 py-3 text-sm text-black`}>
-          <DropDownList data={data} dropdownRef={dropdownRef} handleItemClick={handleItemClick} />
-        </div>
+        <DropDown className={`${dropDownWidth} left-0 top-12`} itemRef={itemRef}>
+          <DropDownList data={data} handleItemClick={handleItemClick} />
+        </DropDown>
       )}
     </div>
   );
 }
 
-export default DropDown;
+export default DropDownBox;
