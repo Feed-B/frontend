c1d82d8ab2c1f5cf0349674c8ec8b5ec1e2ef9df
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-14 00:25:29 +0900

    🐛 Fix: 없어진 아이콘 대체
    fix: 없어진 아이콘 대체

--- app/project/[projectId]/_components/Comment/TotalStar.tsx
+++ app/project/[projectId]/_components/Comment/TotalStar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import starIcon from "@/public/icons/star2.svg";
+import starIcon from "@/public/icons/fullStar.svg";
 
 function TotalStar() {
   return (
@@ -8,7 +8,7 @@ function TotalStar() {
       <p className="mb-1 text-base font-semibold text-[#1C1C1C]">총점</p>
       <div className="flex items-center gap-2">
         <Image src={starIcon} alt="총별점" width={24} />
-        <p className="text-black text-xl font-bold">3.5</p>
+        <p className="text-xl font-bold text-black">3.5</p>
       </div>
     </div>
   );
