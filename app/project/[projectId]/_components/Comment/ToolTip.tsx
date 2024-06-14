a9a419db284549e4dde2659f1ba734886ee08e78
thgus5335 <thgus5335@naver.com>
2024-06-14 19:10:28 +0900

    :bug: Fix: Tooltip 수정
    Fix: Tooltip 수정

--- app/project/[projectId]/_components/Comment/ToolTip.tsx
+++ app/project/[projectId]/_components/Comment/ToolTip.tsx
@@ -6,7 +6,7 @@ const ToolTip = () => {
   return (
     <div className="group relative">
       <Image src={infoIcon} alt="평가 기준 설명." width={24} height={24} />
-      <div className="absolute left-6 translate-y-1 text-nowrap rounded-bl-lg rounded-br-lg rounded-tr-lg bg-gray-800 p-4 text-sm text-white opacity-0 transition-all duration-500 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+      <div className="invisible absolute left-6 top-4 translate-y-1 text-nowrap rounded-bl-lg rounded-br-lg rounded-tr-lg bg-gray-800 p-4 text-sm text-white opacity-0 transition-all duration-500 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
         1. 아이디어 부문은 프로젝트의 독창성을 의미합니다.
         <br />
         2. 디자인 부문은 프로젝트의 사용성과 미적 완성도를 의미합니다.
