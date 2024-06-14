d2a20566f8c21c124112214a0c7f3550cbf0258a
thgus5335 <thgus5335@naver.com>
2024-06-14 16:29:27 +0900

    :sparkles: Feat: 프로젝트 평가 기준 Tooltip 구현
    Feat: 프로젝트 평가 기준 Tooltip 구현

--- app/project/[projectId]/_components/CommentSection/WriteCommentSection.tsx
+++ app/project/[projectId]/_components/CommentSection/WriteCommentSection.tsx
@@ -17,9 +17,20 @@ function WriteCommentSection() {
 
   return (
     <section className="flex flex-col rounded-xl border border-solid border-gray-300 bg-gray-100 p-6">
-      <div className="mb-8 flex gap-2">
+      <div className="mb-8 flex items-center gap-1">
         <p className="text-xl font-semibold text-gray-900">프로젝트를 평가해주세요</p>
-        <Image src={infoIcon} alt="평가 기준 설명." />
+        <div className="group relative">
+          <Image src={infoIcon} alt="평가 기준 설명." width={24} height={24} />
+          <div className="absolute left-6 translate-y-1 text-nowrap rounded-bl-lg rounded-br-lg rounded-tr-lg bg-gray-800 p-4 text-sm text-white opacity-0 transition-all duration-500 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+            1. 아이디어 부문은 프로젝트의 독창성을 의미합니다.
+            <br />
+            2. 디자인 부문은 프로젝트의 사용성과 미적 완성도를 의미합니다.
+            <br />
+            3. 기능은 서비스 요소들의 기능적 완성도와 구현 정도를 의미합니다.
+            <br />
+            4. 완성도는 전반적인 서비스의 만족감과 사용 후 만족감을 의미합니다.
+          </div>
+        </div>
       </div>
       <div className="flex flex-col gap-6">
         <div className="flex w-full justify-between px-3">
