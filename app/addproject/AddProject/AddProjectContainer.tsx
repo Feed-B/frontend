25c01a1ef524bb6f486d14740004d6fde650aa20
JeongSabibi <jsbb528@naver.com>
2024-06-16 00:13:13 +0900

    🎨 Style: 전체적인 width 설정 및 icon 이름 변경, 추가
    Style: 전체적인 width 설정 및 icon 이름 변경, 추가

--- app/addproject/AddProject/AddProjectContainer.tsx
+++ app/addproject/AddProject/AddProjectContainer.tsx
@@ -11,40 +11,40 @@ import Input from "../_components/Input";
 function AddProjectContainer() {
   return (
     <>
-      <div className="mt-8 flex w-[690px] flex-col gap-8">
+      <div className="mt-8 flex w-full flex-col gap-8">
         <section className="flex w-fit flex-col gap-4">
           <Title title="썸네일" additionalSize="(232 x 232px)" />
           <ThumbnailBox />
         </section>
-        <section className="flex flex-col gap-4">
+        <section className="flex w-[690px] flex-col gap-4">
           <Title title="프로젝트 이름" name="projectTitle" additionalSize="(최대 50자)" label />
-          <Input type="text" placeholder="제목을 입력해주세요" name="projectTitle" maxLength={50} inputWidth="w-full" />
+          <Input type="text" placeholder="제목을 입력해주세요" name="projectTitle" maxLength={50} />
         </section>
-        <section className="flex flex-col gap-4">
+        <section className="flex w-[690px] flex-col gap-4">
           <Title title="소개" name="projectIntroduction" additionalSize="(최대 50자)" label />
-          <Input
-            type="text"
-            placeholder="소개를 입력해주세요"
-            name="projectIntroduction"
-            maxLength={50}
-            inputWidth="w-full"
-          />
+          <Input type="text" placeholder="소개를 입력해주세요" name="projectIntroduction" maxLength={50} />
         </section>
-        <section className="flex flex-col gap-4">
+        <section className="flex w-[690px] flex-col gap-4">
           <Title title="본문" name="projectDescription" additionalSize="(최대 150자)" label />
           <TextArea placeholder="텍스트를 입력해주세요" name="projectDescription" maxLength={150} />
         </section>
-        <section className="flex flex-col gap-4">
+        <section className="flex w-[690px] flex-col gap-4">
           <Title title="프로젝트 링크" name="projectLink" label />
-          <Input type="text" placeholder="http://" name="projectLink" inputWidth="w-full" />
+          <Input type="text" placeholder="http://" name="projectLink" />
         </section>
         <section className="flex flex-col gap-4">
           <Title title="이미지" />
           <ProjectImageBox />
         </section>
-        <SkillStackSection />
-        <AddSection title="팀원" placeholder="이름" name="member" inputWidth="w-[114px]" dropDownType="job" />
-        <AddSection title="추가 링크" placeholder="http://" name="addLink" inputWidth="w-[520px]" dropDownType="tool" />
+        <section className="flex w-[690px] flex-col gap-4">
+          <SkillStackSection />
+        </section>
+        <section className="flex w-[690px] flex-col gap-4">
+          <AddSection title="팀원" placeholder="이름" name="member" inputWidth="w-[114px]" dropDownType="job" />
+        </section>
+        <section className="flex w-[690px] flex-col gap-4">
+          <AddSection title="추가 링크" placeholder="http://" name="addLink" dropDownType="tool" />
+        </section>
       </div>
       <div className="mt-56 flex w-full justify-end">
         <Button buttonSize="normal" bgColor="mainBlue">
--- app/addproject/page.tsx
+++ app/addproject/page.tsx
@@ -3,7 +3,7 @@ import AddProjectContainer from "./AddProject/AddProjectContainer";
 
 function page() {
   return (
-    <main className="mx-auto grid max-w-[1200px]">
+    <main className="mx-auto grid w-[1200px]">
       <h1 className="mb-4 mt-6 w-full text-start text-[28px] font-bold text-gray-900">프로젝트 업로드</h1>
       <hr />
       <AddProjectContainer />
