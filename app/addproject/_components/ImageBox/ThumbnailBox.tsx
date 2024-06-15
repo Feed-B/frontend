bbec9c1a44025c0a072b69199305f683c0650c0c
JeongSabibi <jsbb528@naver.com>
2024-06-16 00:15:01 +0900

    ✨ Feat: 썸네일 이미지 업로드 및 수정 구현
    Feat: 썸네일 이미지 업로드 및 수정 구현

--- app/addproject/_components/ImageBox/ThumbnailBox.tsx
+++ app/addproject/_components/ImageBox/ThumbnailBox.tsx
@@ -1,12 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { ChangeEvent, useRef, useState } from "react";
+import Image from "next/image";
 import Button from "@/app/_components/Button/Button";
 
 function ThumbnailBox() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [showImageUrl, setShowImageUrl] = useState<string | null | undefined>("");
+
+  const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setShowImageUrl(imageUrl);
+    }
+  };
+
+  const handleBackgroundDivClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
-    <div className="flex h-[232px] w-[232px] items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-100">
-      <Button className="text-blue-500" buttonSize="normal" bgColor="white">
-        <p className="text-sm font-normal">이미지 업로드</p>
-      </Button>
+    <div className="flex flex-col items-center">
+      <div className="relative flex h-[232px] w-[232px] items-center justify-center rounded-xl border-2 border-dashed border-gray-200 bg-gray-100">
+        {showImageUrl ? (
+          <div className="group relative h-full w-full">
+            <Image fill className="rounded-xl object-cover" src={showImageUrl} alt="썸네일 이미지" />
+            <div className="absolute inset-0 bg-gray-200 opacity-0 transition-opacity duration-300 group-hover:opacity-50" />
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              <Button
+                className="border border-solid border-blue-400 text-blue-500"
+                buttonSize="normal"
+                bgColor="white"
+                onClick={handleBackgroundDivClick}>
+                이미지 수정
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <Button
+            className="border border-solid border-blue-400 text-blue-500"
+            buttonSize="normal"
+            bgColor="white"
+            onClick={handleBackgroundDivClick}>
+            이미지 업로드
+          </Button>
+        )}
+      </div>
+      <input type="file" id="fileInput" ref={fileInputRef} className="hidden" onChange={handleImageChange} />
     </div>
   );
 }
