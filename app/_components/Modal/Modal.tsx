6d2c0b71b09f971ab43b29b23aff40d15e9283fe
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-17 15:29:20 +0900

    💄 Design: 모달 공통컴포넌트 UI 수정

--- app/_components/Modal/Modal.tsx
+++ app/_components/Modal/Modal.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 import { twMerge } from "tailwind-merge";
 import ModalPortal from "@/app/_utils/ModalPortal";
+import closeIcon from "@/public/icons/close.svg";
 
 interface ModalProps extends PropsWithChildren {
   openModal: boolean;
@@ -12,7 +14,7 @@ interface ModalProps extends PropsWithChildren {
 
 function Modal({ children, openModal, handleModalClose, className }: ModalProps) {
   const modalClass = twMerge(
-    "fixed top-1/2 left-1/2 bg-white rounded-lg z-50 transform -translate-x-1/2 -translate-y-1/2 px-28 py-32 mobile:px-20 mobile:py-28",
+    "fixed top-1/2 left-1/2 bg-white rounded-lg z-50 transform -translate-x-1/2 -translate-y-1/2 mobile:px-20 mobile:py-28",
     className
   );
 
@@ -22,6 +24,15 @@ function Modal({ children, openModal, handleModalClose, className }: ModalProps)
     <ModalPortal>
       <div className="fixed left-0 top-0 z-50 h-full w-full bg-black-overlay" onClick={handleModalClose}>
         <div onClick={event => event.stopPropagation()} className={`${modalClass} ${openModal ? open : ""}`}>
+          <Image
+            src={closeIcon}
+            alt="닫기 아이콘"
+            width={32}
+            height={32}
+            priority
+            className="absolute right-0 top-0 my-4 mr-4 cursor-pointer"
+            onClick={handleModalClose}
+          />
           {children}
         </div>
       </div>
