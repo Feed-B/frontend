a03f04659de4b96f908f86b1d83915833d75cbc2
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-17 21:49:00 +0900

    🎨 Style: API 로직 수정
    style: API 로직 수정

--- app/_apis/comment.ts
+++ app/_apis/comment.ts
@@ -1,29 +1,16 @@
 import httpClient from "./httpClient";
 import { CommentDetailResponse, CommentsListResponse, ReflyCommentResponse } from "./schema/commentResponse";
 
-interface CommentsListRequest {
-  projectId: string;
-  page?: number;
-  size?: number;
-  limit?: number;
-}
-
-interface ReflyCommentListRequest extends CommentsListRequest {
-  commentId: string;
-}
-
 export const commentApi = {
-  getCommentList: async ({ projectId, page = 1, size = 10, limit = 100 }: CommentsListRequest) => {
-    await httpClient().get<CommentsListResponse>(`/${projectId}/commnets`, { page, size, limit });
+  getCommentList: async (projectId: number, page = 1, size = 10, limit = 100) => {
+    await httpClient().get<CommentsListResponse>(`/${projectId}/comments?page=${page}&size=${size}&limit=${limit}`);
   },
   getCommentDetail: async (projectId: string, commentId: string) => {
-    await httpClient().get<CommentDetailResponse>(`/projects/${projectId}/commnets/${commentId}`);
+    await httpClient().get<CommentDetailResponse>(`/projects/${projectId}/comments/${commentId}`);
   },
-  getReflyCommentList: async ({ projectId, commentId, page = 1, size = 10, limit = 100 }: ReflyCommentListRequest) => {
-    await httpClient().get<ReflyCommentResponse>(`/projects/${projectId}/commnets/${commentId}/replies`, {
-      page,
-      size,
-      limit,
-    });
+  getReflyCommentList: async (projectId: number, commentId: number, page = 1, size = 10, limit = 100) => {
+    await httpClient().get<ReflyCommentResponse>(
+      `/projects/${projectId}/comments/${commentId}/replies?page=${page}&size=${size}&limit=${limit}`
+    );
   },
 };
