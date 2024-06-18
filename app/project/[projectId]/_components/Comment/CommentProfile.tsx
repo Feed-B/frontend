c7e45cd34e683cea545cc049787e563ee6e1de8e
thgus5335 <thgus5335@naver.com>
2024-06-19 03:05:50 +0900

    :lipstick: Design: CommentProfile 디자인 수정
    Design: CommentProfile 디자인 수정

--- app/project/[projectId]/_components/Comment/CommentProfile.tsx
+++ app/project/[projectId]/_components/Comment/CommentProfile.tsx
@@ -4,10 +4,10 @@ import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
 function CommentProfile() {
   return (
     <div className="flex items-center gap-2">
-      <ProfileImage imageUrl={""} className="h-10 w-10" />
+      <ProfileImage imageUrl="defalut" className="h-10 w-10" />
       <div>
-        <p className="text-sm">김한주</p>
-        <p className="text-[10px] font-normal text-blue-400">프론트엔드</p>
+        <p className="text-sm font-semibold">일이삼사오육칠팔</p>
+        <p className="text-[10px] text-blue-500">프론트엔드</p>
       </div>
     </div>
   );
