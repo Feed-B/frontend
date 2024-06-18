f4f74f38e565bc2d6c2108d5b84da81fca17fc1a
thgus5335 <thgus5335@naver.com>
2024-06-18 14:06:26 +0900

    :lipstick: Design: 댓글 작성 글자 수 UI 추가
    Design: 댓글 작성 글자 수 UI 추가

--- app/project/[projectId]/_components/Comment/WriteComment.tsx
+++ app/project/[projectId]/_components/Comment/WriteComment.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@/app/_components/Button/Button";
 
 const MAX_COMMENT_LIMIT = 150;
 
 const WriteComment = () => {
+  const [comment, setComment] = useState("");
+
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(event.target.value);
+  };
+
   return (
     <>
-      <div className="flex h-32 gap-1 rounded-lg border border-solid border-gray-200 bg-white p-3 text-gray-500">
+      <div className="flex h-32 gap-1 rounded-lg border border-solid border-gray-200 bg-white p-3">
         <textarea
-          className="h-full w-full resize-none bg-white text-sm text-black outline-none placeholder:text-gray-500"
+          onChange={onChange}
+          className="h-full w-full resize-none bg-white text-sm text-gray-600 outline-none placeholder:text-gray-500"
           placeholder="댓글을 입력해주세요"
           maxLength={MAX_COMMENT_LIMIT}
         />
-        <p className="mt-auto text-nowrap text-sm text-gray-500">0/{MAX_COMMENT_LIMIT}</p>
+        <p className="mt-auto text-nowrap text-sm text-gray-500">
+          {comment.length}/{MAX_COMMENT_LIMIT}
+        </p>
       </div>
       <Button className="ml-auto" buttonSize="small" bgColor="mainBlue">
         등록
