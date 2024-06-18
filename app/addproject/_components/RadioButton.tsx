c64da878037124d0a37e0b69c949fbf6e61696a0
JeongSabibi <jsbb528@naver.com>
2024-06-18 16:30:29 +0900

    🎨 Style: 코드리뷰 반영
    Style: 코드리뷰 반영

--- app/addproject/_components/RadioButton.tsx
+++ app/addproject/_components/RadioButton.tsx
@@ -1,10 +1,6 @@
 import React, { InputHTMLAttributes } from "react";
 
-interface RadioButtonProps extends InputHTMLAttributes<HTMLInputElement> {
-  text: string;
-}
-
-function RadioButton({ value, checked, onChange, text }: RadioButtonProps) {
+function RadioButton({ value, checked, onChange }: InputHTMLAttributes<HTMLInputElement>) {
   const RadioButtonStyle =
     "shadow-default-radio-border checked:shadow-check-radio-border h-3 w-3 appearance-none rounded-full bg-gray-200 checked:border-2 checked:border-white checked:bg-gray-800 hover:cursor-pointer";
 
@@ -18,7 +14,7 @@ function RadioButton({ value, checked, onChange, text }: RadioButtonProps) {
         onChange={onChange}
         className={RadioButtonStyle}
       />
-      <span className="ml-1 text-xs font-medium text-[#4D5256]">{text}</span>
+      <span className="ml-1 text-xs font-medium text-gray-900">{value}</span>
     </label>
   );
 }
