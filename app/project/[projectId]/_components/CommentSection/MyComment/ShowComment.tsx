1f7418fd5777112bf05cd787b3a595114ef9f5a8
thgus5335 <thgus5335@naver.com>
2024-06-19 03:52:09 +0900

    :lipstick: Design: 내가 쓴 댓글 UI 수정
    Design: 내가 쓴 댓글 UI 수정

--- app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
+++ app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import kebabIcon from "@/public/icons/kebab.svg";
 import DropDown from "@/app/_components/DropDown/DropDown";
 import useToggleHook from "@/app/_hooks/useToggleHook";
+import arrowIcon from "@/public/icons/blackArrowRight.svg";
 import CommentProfile from "../../Comment/CommentProfile";
 import CommentCount from "../../Comment/CommentCount";
 import TotalStar from "../../Comment/TotalStar";
@@ -18,20 +19,12 @@ function ShowComment() {
   return (
     <>
       <h3 className="mb-4 text-lg font-semibold">내가 쓴 글</h3>
-      <div className="relative flex flex-col gap-4 rounded-xl border border-solid border-gray-300 bg-gray-100 p-6">
+      <div className="relative flex flex-col gap-4 rounded-xl border border-solid border-gray-300 bg-gray-100 p-4">
         <div className="flex justify-between">
           <CommentProfile />
           <div className="flex items-center gap-2">
             <CommentCount size="large" />
-            <Image
-              className="relative"
-              src={kebabIcon}
-              alt="프로젝트 메뉴."
-              width={24}
-              height={32}
-              priority
-              onClick={toggleState}
-            />
+            <Image className="relative" src={kebabIcon} alt="댓글 메뉴." width={24} height={32} onClick={toggleState} />
             {isOpen && (
               <DropDown className="w-fit translate-x-4 translate-y-16">
                 <DropDown.LinkItem href={`/project/${projectId}/edit`}>수정</DropDown.LinkItem>
@@ -40,10 +33,13 @@ function ShowComment() {
             )}
           </div>
         </div>
-        <p className="h-8 text-sm text-gray-900">댓글입니다.</p>
+        <p className="text-overflow-3 h-14 text-sm text-gray-900">댓글 내용입니다.</p>
         <div className="flex justify-between">
           <TotalStar />
-          {/* 더보기 */}
+          <div className="flex items-center">
+            <p className="text-xs text-gray-900">더보기</p>
+            <Image src={arrowIcon} alt="댓글 상세보기." width={20} />
+          </div>
         </div>
       </div>
     </>
