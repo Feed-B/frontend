2ba3c971edf9e7b6a6c333449089210bb3dca219
codingaring <sksms2104@naver.com>
2024-06-19 01:27:28 +0900

    🎨 Style: 파일리스트에 대한 처리 제거

--- app/_hooks/useFileInput.ts
+++ app/_hooks/useFileInput.ts
@@ -13,15 +13,6 @@ const useHandleInputFile = () => {
     }
   };
 
-  const handleImageListChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-
-    if (file) {
-      const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl);
-    }
-  };
-
   const handleRemoveImage = () => {
     setImage("");
   };
@@ -32,7 +23,7 @@ const useHandleInputFile = () => {
     }
   };
 
-  return { inputRef, image, handleImageChange, handleImageListChange, handleRemoveImage, handleSelectImageClick };
+  return { inputRef, image, handleImageChange, handleRemoveImage, handleSelectImageClick };
 };
 
 export default useHandleInputFile;
