c11853354b56b41aa10eede1ffde936277e2fb98
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-19 17:39:00 +0900

    ✨ Feat: 각 컴포넌트 context 함수 연결
    feat: 각 컴포넌트 context 함수 연결

--- app/main/_components/SearchBar/SearchBar.tsx
+++ app/main/_components/SearchBar/SearchBar.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Image from "next/image";
 import searchIcon from "@/public/icons/search.svg";
-import { SearchProject } from "./SearchAction";
+import { useGetStack } from "../../_context/StackProvider";
 
 function SearchBar() {
+  const { projectState, isChangeSearchString } = useGetStack();
+  const [inputValue, setInputValue] = useState(projectState.searchString);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    isChangeSearchString(inputValue);
+  };
+
   return (
     <div className="flex h-8 w-48 items-center gap-2 rounded-[20px] border border-solid border-[#D6D6D6] p-2">
       <Image src={searchIcon} alt="검색 기능입니다." width={16} priority />
-      <form action={SearchProject}>
+      <form onSubmit={handleSubmit}>
         <input
           className="mb-1 w-36 text-xs font-normal outline-none"
           name="search"
           placeholder="프로젝트 이름 검색하기"
           autoComplete="off"
+          value={inputValue}
+          onChange={onChange}
         />
       </form>
     </div>
