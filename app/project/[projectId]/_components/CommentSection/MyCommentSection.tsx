9707318bba7e73eb3da4846f6a95ae1d981a1b0f
thgus5335 <thgus5335@naver.com>
2024-06-19 17:54:30 +0900

    :art: Style: MyComment 컨텍스트 구현
    Style: MyComment 컨텍스트 구현

--- app/project/[projectId]/_components/CommentSection/MyCommentSection.tsx
+++ app/project/[projectId]/_components/CommentSection/MyCommentSection.tsx
@@ -1,14 +1,30 @@
+"use client";
+
 import React from "react";
+import MyCommentProvider from "../../_context/MycommentProvider";
+import { useMyCommentContext } from "../../_context/MycommentProvider";
 import ShowComment from "./MyComment/ShowComment";
-// import WriteComment from "./MyComment/WriteComment";
-// import EditComment from "./MyComment/EditComment";
+import WriteComment from "./MyComment/WriteComment";
+import EditComment from "./MyComment/EditComment";
+
+const CommentContainer = () => {
+  const { view } = useMyCommentContext();
+
+  return (
+    <section>
+      {view === "show" && <ShowComment />}
+      {view === "write" && <WriteComment />}
+      {view === "edit" && <EditComment />}
+    </section>
+  );
+};
 
 function MyCommentSection() {
   return (
     <section>
-      <ShowComment />
-      {/* <WriteComment /> */}
-      {/* <EditComment /> */}
+      <MyCommentProvider>
+        <CommentContainer />
+      </MyCommentProvider>
     </section>
   );
 }
