45302446ed26cb56ddde8a2bc568405fa1943740
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-21 01:26:20 +0900

    💄 Design: empty card 디자인 수정
    design: empty card 디자인 수정

--- app/_components/ProjectList/ProjectCard/EmptyCard.tsx
+++ app/_components/ProjectList/ProjectCard/EmptyCard.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import emptyFileIcon from "@/public/icons/emptyFile.svg";
+import { useGetStack } from "@/app/main/_context/StackProvider";
 
 function EmptyCard() {
+  const { projectState } = useGetStack();
+
   return (
     <div className="absolute left-1/2 flex -translate-x-1/2  flex-col items-center justify-center gap-6">
       <Image width={54} height={72} src={emptyFileIcon} alt="파일 없음" />
-      <p className="text-xl font-semibold text-[#8B8B8B]">프로젝트 목록이 없어요.</p>
+      <p className="text-xl font-semibold text-gray-700">
+        <span className="text-xl font-semibold text-blue-600">`{projectState.searchString}`</span>에 대한 검색결과가
+        없어요
+      </p>
     </div>
   );
 }
