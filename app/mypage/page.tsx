da1448291ca46c4aa2918cece541def5a9002bb4
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-21 00:51:36 +0900

    🐛 Fix: 충돌 예상되는 로직 제거
    fix: 충돌 예상되는 로직 제거

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,28 +1,15 @@
 "use client";
 import { MouseEvent, useState } from "react";
-import { myProjectList, wishProjectList } from "../_components/ProjectList/mockDataCardList";
-import ProjectList from "../_components/ProjectList/ProjectList";
-import { ProjectListResponse } from "../_types/ProjectListDataType";
 import MyPageCategory from "./_components/MyPageCategory";
 import Profile from "./_components/Profile";
 
 function MyPage() {
   const [selectCategory, setSelectCategory] = useState("myProject");
-  let projectListData: ProjectListResponse[] = [];
 
   const handleSelectCategory = (event: MouseEvent<HTMLButtonElement>) => {
     setSelectCategory(event.currentTarget.id);
   };
 
-  switch (selectCategory) {
-    case "myProject": {
-      projectListData = myProjectList.data;
-      break;
-    }
-    case "wishProject": {
-      projectListData = wishProjectList.data;
-    }
-  }
   return (
     <main className="mb-20 ml-[50%] mt-10 flex w-[1200px] -translate-x-1/2 gap-11 gap-y-16">
       <div className="w-[180px]">
@@ -30,7 +17,6 @@ function MyPage() {
       </div>
       <div className="flex w-[976px] flex-col gap-8">
         <Profile />
-        <ProjectList projectList={projectListData} />
       </div>
     </main>
   );
