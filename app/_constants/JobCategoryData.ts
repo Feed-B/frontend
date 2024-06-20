4e8e83e25f679f47e2188f270f83160a6a4746f7
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-20 16:59:46 +0900

    ♻️ Refactor: JOB_CATEGORIES 값 임시로 영어 변경

--- app/_constants/JobCategoryData.ts
+++ app/_constants/JobCategoryData.ts
@@ -1,14 +1,14 @@
 const JOB_CATEGORIES = {
-  ALL: "전체",
-  FRONTEND: "프론트엔드",
-  BACKEND: "백엔드",
-  DESIGNER: "디자이너",
+  ALL: "ALL",
+  FRONTEND: "FRONTEND",
+  BACKEND: "BACKEND",
+  DESIGNER: "DESIGNER",
   IOS: "IOS",
-  ANDROID: "안드로이드",
-  DEVOPS: "데브옵스",
-  PLANNER: "기획자",
-  FULLSTACK: "풀스택",
-  ETC: "기타",
+  ANDROID: "ANDROID",
+  DEVOPS: "DEVOPS",
+  PLANNER: "PLANNER",
+  FULLSTACK: "FULLSTACK",
+  ETC: "ETC",
 };
 
 export default JOB_CATEGORIES;
