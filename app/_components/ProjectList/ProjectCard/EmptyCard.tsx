59271cec98734ba74152a6dc9682635d0c0ff480
yu kyoung kim <sksms2104@naver.com>
2024-06-21 18:10:42 +0900

    💄 Design: EmptyCard 상단 여백 추가

--- app/_components/ProjectList/ProjectCard/EmptyCard.tsx
+++ app/_components/ProjectList/ProjectCard/EmptyCard.tsx
@@ -8,7 +8,7 @@ function EmptyCard() {
   const { projectState } = useGetStack();
 
   return (
-    <div className="absolute left-1/2 flex -translate-x-1/2  flex-col items-center justify-center gap-6">
+    <div className="absolute left-1/2 top-40 flex -translate-x-1/2 flex-col items-center justify-center gap-6">
       <Image width={54} height={72} src={emptyFileIcon} alt="파일 없음" />
 
       <p className="text-xl font-semibold text-gray-700">
