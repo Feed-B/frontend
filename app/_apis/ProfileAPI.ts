5e3ff920cd49a50039c35dfd35143c61b10504dc
yu kyoung kim <sksms2104@naver.com>
2024-06-21 23:09:15 +0900

    ✨ Feat: profile 데이터 패칭 함수 연결

--- app/_apis/ProfileAPI.ts
+++ app/_apis/ProfileAPI.ts
@@ -4,8 +4,22 @@ interface CurrentUserIdType {
   id: number;
 }
 
+export interface UserProfileType {
+  id: number;
+  email: string;
+  nickName: string;
+  aboutMe: string;
+  job: JobType;
+  imageUrl: string;
+}
+
+export type JobType = "FRONTEND" | "BACKEND" | "DESIGNER" | "ANDROID" | "IOS" | "DEVOPS";
+
 export const profileAPI = {
   getCurrentUserId: async () => {
     return await httpClient().get<CurrentUserIdType>("/profile");
+  },
+  getUserProfile: async ({ userId }: { userId: number }) => {
+    return await httpClient().get<UserProfileType>(`/profile/${userId}`);
   },
 };
