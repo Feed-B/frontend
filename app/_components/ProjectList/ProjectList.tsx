06b150c2760601d511f39c9d4ec9a426e55a98fa
yu kyoung kim <sksms2104@naver.com>
2024-06-21 21:56:15 +0900

    🎨 Style: observer target변경

--- app/_components/ProjectList/ProjectList.tsx
+++ app/_components/ProjectList/ProjectList.tsx
@@ -16,19 +16,21 @@ function ProjectList({ projectList, lastRef }: ProjectListProp) {
   return (
     <div className="relative grid grid-cols-4 gap-4">
       {projectList && projectList.length > 0 ? (
-        projectList.map((project, index) => (
-          <Link
-            href={`/project/${project.projectId}`}
-            className="flex cursor-pointer flex-col gap-2.5"
-            key={project.projectId}>
-            <ProjectCard project={project} />
-            <ProjectCardInfo
-              ref={projectList.length - 1 === index ? lastRef : null}
-              projectTitle={project.projectTitle}
-              projectSubDescription={project.introduction}
-              viewCount={formatViewCount(project.viewCount, 9999)}
-            />
-          </Link>
+        projectList.map(project => (
+          <>
+            <Link
+              href={`/project/${project.projectId}`}
+              className="flex cursor-pointer flex-col gap-2.5"
+              key={project.projectId}>
+              <ProjectCard project={project} />
+              <ProjectCardInfo
+                projectTitle={project.projectTitle}
+                projectSubDescription={project.introduction}
+                viewCount={formatViewCount(project.viewCount, 9999)}
+              />
+            </Link>
+            <div className="absolute bottom-0" ref={lastRef} />
+          </>
         ))
       ) : (
         <EmptyCard />
