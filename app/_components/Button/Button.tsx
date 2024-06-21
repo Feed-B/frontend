663113d4e49d8243e6e38b28ed81ec9970e322bb
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-21 14:34:05 +0900

    ✨ Feat: bgColorCLasses에 gray 추가

--- app/_components/Button/Button.tsx
+++ app/_components/Button/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, PropsWith
 
 type ButtonSize = "normal" | "small";
 
-type BgColor = "white" | "stroke" | "naver" | "yellow" | "kakao" | "mainBlue";
+type BgColor = "yellow" | "white" | "gray" | "stroke" | "naver" | "kakao" | "mainBlue";
 
 const buttonClasses = {
   normal: "min-w-20 h-11 rounded-lg px-4 py-3 text-sm",
@@ -21,6 +21,7 @@ const bgColorClasses: Record<BgColor, string> = {
   yellow: "bg-yellow-500 hover:bg-yellow-600 text-black",
   stroke: "border border-gray-200 bg-white",
   white: "border border-gray-200 bg-white",
+  gray: "border border-gray-200 bg-gray-100",
   naver: "bg-[#03C75A] text-white",
   kakao: "bg-[#FEE500]",
   mainBlue: "bg-blue-500",
