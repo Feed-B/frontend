18a8bda73c401d28f2f02168fe2a491b405afc6c
thgus5335 <thgus5335@naver.com>
2024-06-23 04:21:01 +0900

    :art: Style: 프로젝트 이미지 슬라이드로 변경 및 애니메이션 적용
    Style: 프로젝트 이미지 슬라이드로 변경 및 애니메이션 적용

--- app/_hooks/useImageIndex.ts
+++ app/_hooks/useImageIndex.ts
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 const useImageIndex = () => {
   const [index, setIndex] = useState(0);
+  const [translate, setTranslate] = useState(0);
 
-  const handlePrev = () => {
+  const handlePrev = (width: number) => {
     setIndex(prevIndex => Math.max(prevIndex - 1, 0));
+    setTranslate((index - 1) * width);
   };
 
-  const handleNext = (imageListLength: number) => {
+  const handleNext = (width: number, imageListLength: number) => {
     setIndex(prevIndex => Math.min(prevIndex + 1, imageListLength - 1));
+    setTranslate((index + 1) * width);
   };
 
-  return { index, handlePrev, handleNext };
+  return { index, translate, handlePrev, handleNext };
 };
 
 export default useImageIndex;
