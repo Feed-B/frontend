e297357113b39bc47abf12ce505dea3872ee404b
thgus5335 <thgus5335@naver.com>
2024-06-22 19:48:15 +0900

    :sparkles: Feat: 프로젝트 article API 연결 구현
    Feat: 프로젝트 article API 연결 구현

--- app/project/[projectId]/_components/ProjectSection/LinkSection.tsx
+++ app/project/[projectId]/_components/ProjectSection/LinkSection.tsx
@@ -6,7 +6,7 @@ import { TOOL_DATA } from "@/app/_constants/ToolData";
 interface LinkListProps {
   linkList: {
     id: number;
-    tool: string;
+    siteType: string;
     url: string;
   }[];
 }
@@ -17,7 +17,7 @@ function LinkSection({ linkList }: LinkListProps) {
       <p className="mb-2 text-sm font-semibold">추가 링크</p>
       <div className="flex gap-2">
         {linkList.map(link => {
-          const linkItem = TOOL_DATA.find(item => item.name === link.tool);
+          const linkItem = TOOL_DATA.find(item => item.name === link.siteType);
           return (
             <div className="rounded-full border border-solid border-gray-900 p-0.5" key={link.id}>
               <Link href={`${link.url}`} target="_blank">
