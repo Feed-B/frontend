60a8d053c8e82637b246847fea107808327b0554
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-23 00:31:44 +0900

    ✨ Feat: 인증관리와 회원가입 모달 처리 추가
    useState를 이용하여 인증 상태를 관리하고, zustand 스토어를 사용하여 type 상태를 관리하며, 토큰이 존재할 경우 인증 상태를 설정, 로그아웃 시 토큰을 삭제하고 인증 상태를 업데이트하는 기능 추가

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,31 +1,69 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import ExampleButton from "@/app/signup/_components/ExampleButton/ExampleButton";
 import uploadIcon from "@/public/icons/upload.svg";
+import useModal from "@/app/_hooks/useModal";
+import { useKakaoStore } from "@/app/_utils/zustandStore";
+import { getToken, removeToken } from "@/app/_utils/handleToken";
+import feedbee from "@/public/icons/feedbee.svg";
+import logoTextIcon from "@/public/icons/logoText.svg";
+import LoginButton from "../LoginButton/LoginButton";
 import Button from "../Button/Button";
+import SignUpModal from "../SignUpModal/SignUpModal";
 import HeaderDropDownBox from "./HeaderDropDownBox";
 
 function Header() {
+  const { openModal: isSignUpModal, handleModalClose: signUpModalClose, handleModalOpen: signUpModalOpen } = useModal();
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const { type, setType } = useKakaoStore();
+
+  const handleLogout = () => {
+    removeToken();
+    setIsAuthenticated(false);
+  };
+
+  useEffect(() => {
+    const token = getToken();
+    if (token && token.accessToken) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (type === "signUp") {
+      signUpModalOpen();
+      setType("");
+    }
+  }, [type, signUpModalOpen, setType]);
+
   return (
     <header className="h-16 w-full border-b border-solid border-gray-300 py-2 text-white">
       <div className="relative m-0 mx-auto flex h-11 max-w-[1400px] items-center justify-between">
-        <Link href="/main">
-          <h1 className="h-full text-black">LOGO</h1>
+        <Link href="/main" className="flex items-center gap-2.5">
+          <Image src={feedbee} width={33} height={32} alt="로고 아이콘" />
+          <Image src={logoTextIcon} alt="로고 텍스트" />
         </Link>
         <div className="flex h-full items-center gap-4">
-          <Link href="/addproject">
-            <Button buttonSize="normal" bgColor="yellow" className="flex items-center justify-center gap-1">
-              <Image src={uploadIcon} alt="프로젝트 업로드" width={20} priority />
-              <span>업로드</span>
-            </Button>
-          </Link>
-          <ExampleButton />
-          <HeaderDropDownBox />
+          {isAuthenticated ? (
+            <>
+              <Link href="/addproject">
+                <Button buttonSize="normal" bgColor="yellow" className="flex items-center justify-center gap-1">
+                  <Image src={uploadIcon} alt="프로젝트 업로드" width={20} priority />
+                  <span>업로드</span>
+                </Button>
+              </Link>
+              <HeaderDropDownBox handleLogout={handleLogout} />
+            </>
+          ) : (
+            <LoginButton />
+          )}
         </div>
       </div>
+      <SignUpModal openModal={isSignUpModal} handleModalClose={signUpModalClose} dataType="job" />
     </header>
   );
 }
