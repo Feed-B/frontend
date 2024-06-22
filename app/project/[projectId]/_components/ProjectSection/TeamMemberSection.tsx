925a076392715a1848aea6b65ee1c51ed687b69a
thgus5335 <thgus5335@naver.com>
2024-06-23 06:05:41 +0900

    :art: Style: 팀원 직무 한글로 변경
    Style: 팀원 직무 한글로 변경

--- app/project/[projectId]/_components/ProjectSection/TeamMemberSection.tsx
+++ app/project/[projectId]/_components/ProjectSection/TeamMemberSection.tsx
@@ -5,10 +5,13 @@ import Link from "next/link";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { TeamMemberResponse } from "@/app/_apis/schema/projectResponse";
 import { projectQueryKeys } from "@/app/_queryFactory/projectQuery";
+import { JOB_CATEGORIES_KR } from "@/app/_constants/JobCategoryData";
 
 interface Props {
   projectId: number;
 }
+
+type JobCategory = keyof typeof JOB_CATEGORIES_KR;
 
 function TeamMemberSection({ projectId }: Props) {
   const { data: teamMember }: UseQueryResult<TeamMemberResponse, Error> = useQuery(
@@ -24,7 +27,7 @@ function TeamMemberSection({ projectId }: Props) {
       <div className="flex gap-6">
         {teamMember.map((team, index) => (
           <div className="flex gap-3" key={index}>
-            <p className="p-1 text-sm font-semibold text-blue-400">{team.job}</p>
+            <p className="p-1 text-sm font-semibold text-blue-400">{JOB_CATEGORIES_KR[team.job as JobCategory]}</p>
             <div className="flex items-center gap-2 text-sm text-gray-900">
               {team.teammateList.map(member => (
                 <div key={member.id}>
