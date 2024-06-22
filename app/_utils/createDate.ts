4c37d7f5bf77d05e5a342fd758f876fc77736b27
thgus5335 <thgus5335@naver.com>
2024-06-22 19:24:52 +0900

    :sparkles: Feat: createDate 유틸 함수 구현
    Feat: createDate 유틸 함수 구현

---
+++ app/_utils/createDate.ts
@@ -0,0 +1,7 @@
+export const createDate = (value: string) => {
+  const date = new Date(value);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}.${month}.${day}`;
+};
