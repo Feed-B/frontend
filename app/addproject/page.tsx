dfe6325aed3c35845e92988507ec54e07cb93090
JeongSabibi <jsbb528@naver.com>
2024-06-22 21:19:06 +0900

    ✨ Feat: page에 HydrationBoundary 추가
    Feat: page에 HydrationBoundary 추가

--- app/addproject/page.tsx
+++ app/addproject/page.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import getQueryClient from "../_queryFactory/getQueryClient";
 import AddProjectContainer from "./AddProject/AddProjectContainer";
 
 function page() {
+  const queryClient = getQueryClient();
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <main className="mx-auto grid w-[1200px]">
-      <h1 className="mb-4 mt-16 w-full text-start text-[28px] font-bold text-gray-900">프로젝트 업로드</h1>
-      <hr />
-      <AddProjectContainer />
-    </main>
+    <HydrationBoundary state={dehydratedState}>
+      <main className="mx-auto grid w-[1200px]">
+        <h1 className="mb-4 mt-16 w-full text-start text-[28px] font-bold text-gray-900">프로젝트 업로드</h1>
+        <hr />
+        <AddProjectContainer />
+      </main>
+    </HydrationBoundary>
   );
 }
 
