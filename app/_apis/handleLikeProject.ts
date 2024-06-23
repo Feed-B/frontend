8f3560fc4210c11b2a800ca920a459131204ad46
yu kyoung kim <sksms2104@naver.com>
2024-06-24 02:24:31 +0900

    ✨ Feat: 찜 버튼 api 연결

--- app/_apis/handleLikeProject.ts
+++ app/_apis/handleLikeProject.ts
@@ -3,9 +3,9 @@ import { headers } from "./projectListAPI";
 
 export const handleLikeProject = {
   postLikeProject: async ({ projectId }: { projectId: number }) => {
-    return await httpClient().post(`/project/${projectId}/like`, { "": "" }, headers.headers);
+    return await httpClient().post(`/projects/${projectId}/like`, { "": "" }, headers.headers);
   },
   deleteLikeProject: async ({ projectId }: { projectId: number }) => {
-    return await httpClient().delete(`/project/${projectId}/unlike`, headers.headers);
+    return await httpClient().delete(`/projects/${projectId}/unlike`, headers.headers);
   },
 };
