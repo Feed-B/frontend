d4a600d886bd5a953b92fd0b526d364612ad64cc
yu kyoung kim <sksms2104@naver.com>
2024-06-23 10:26:04 +0900

    🎨 Style: profile 스켈레톤 UI 추가 및 tanstack-query

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MyPageContent from "./_components/MyPageContent";
 
 function MyPage() {
