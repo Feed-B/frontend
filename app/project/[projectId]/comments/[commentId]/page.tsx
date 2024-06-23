ae55f845a895ec0d98be404cbee22b1571d0ba9c
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-23 18:50:15 +0900

    ✨ Feat: 대댓글 리스트 받아오는 API 연결
    feat: 대댓글 리스트 받아오는 API 연결

--- app/project/[projectId]/comments/[commentId]/page.tsx
+++ app/project/[projectId]/comments/[commentId]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import ScrollToTopButton from "@/app/_components/Button/DirectionButton";
 import { commentQueryKeys } from "@/app/_queryFactory/commentQuery";
+import { commentApi } from "@/app/_apis/comment";
 import CommentInput from "./_components/CommentInput";
 import ReflyCommentList from "./_components/ReflyCommentList";
 import CommentSection from "./_components/CommentSection";
@@ -17,6 +18,26 @@ async function CommentPage({ params }: Props) {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery(commentQueryKeys.detail(params.projectId, params.commentId));
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["comment", "reflyList", "reflyCommentList"],
+    queryFn: async () => {
+      const response = commentApi.getReflyCommentList({
+        projectId: params.projectId,
+        commentId: params.commentId,
+        page: 1,
+        size: 12,
+      });
+      return response;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: any) => {
+      const { customPageable } = lastPage;
+      if (customPageable.hasNext) {
+        return customPageable.page + 1; // 다음 페이지 번호 반환
+      }
+      return undefined; // 더 이상 페이지가 없으면 undefined 반환
+    },
+  });
 
   const dehydratedState = dehydrate(queryClient);
 
@@ -28,7 +49,7 @@ async function CommentPage({ params }: Props) {
         <section className="mt-12">
           <CommentInput />
         </section>
-        <ReflyCommentList />
+        <ReflyCommentList projectId={params.projectId} commentId={params.commentId} />
       </div>
     </HydrationBoundary>
   );
