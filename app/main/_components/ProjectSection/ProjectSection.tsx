a3aa8a44dfc0b13c9d886e58635377206759f2cd
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-24 02:49:22 +0900

    🎨 Style: 타입 제거
    style: 타입 제거

--- app/main/_components/ProjectSection/ProjectSection.tsx
+++ app/main/_components/ProjectSection/ProjectSection.tsx
@@ -13,7 +13,7 @@ function ProjectSection() {
 
   const { data, fetchNextPage } = useInfiniteQuery({
     queryKey: ["project", "list", "projectList"],
-    queryFn: ({ pageParam = 1 }) => projectApi.getProjectList({ ...projectState, page: pageParam as number }),
+    queryFn: ({ pageParam = 1 }) => projectApi.getProjectList({ ...projectState, page: pageParam }),
     initialPageParam: 1,
     getNextPageParam: lastPage => {
       const { customPageable } = lastPage;
