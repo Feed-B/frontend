c93164a705c66a80b4354b36e5066fe6b7aee993
yu kyoung kim <sksms2104@naver.com>
2024-06-24 02:43:01 +0900

    ♻️ Refactor: prop 묶어서 전달

--- app/_components/ProjectList/ProjectCard/HoverCard.tsx
+++ app/_components/ProjectList/ProjectCard/HoverCard.tsx
@@ -1,28 +1,23 @@
+import { ProjectData } from "@/app/_apis/schema/projectResponse";
 import WishButtonAndCount from "../../WishButtonAndCount/WishButtonAndCount";
 import StackBadge from "./StackBadge";
 
-function HoverCard({
-  stackList,
-  wishCount,
-  isWishProject,
-  projectId,
-}: {
-  stackList: string[];
-  wishCount: number;
-  isWishProject: boolean;
-  projectId: number;
-}) {
+function HoverCard({ project }: { project: ProjectData }) {
   return (
     <div className="absolute inset-0 z-30 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
       <div className="flex p-4">
         <div className="z-20">
           <div className="flex flex-wrap gap-1">
-            {stackList.map((stack, index) => (
+            {project.stackList.map((stack, index) => (
               <StackBadge key={index} stack={stack} />
             ))}
           </div>
           <div className="absolute bottom-3 right-3 text-white">
-            <WishButtonAndCount projectId={projectId} isFavorite={isWishProject} wishCount={wishCount} />
+            <WishButtonAndCount
+              projectId={project.projectId}
+              isFavorite={project.isLiked}
+              wishCount={project.likeCount}
+            />
           </div>
         </div>
         <div className="absolute inset-0 z-10 bg-gradient-to-t from-black to-[rgba(0,0,0,0)] opacity-90" />
