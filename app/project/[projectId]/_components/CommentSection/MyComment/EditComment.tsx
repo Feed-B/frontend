fafb48ed45d64412b798b9dc16a0a50d51496680
thgus5335 <thgus5335@naver.com>
2024-06-24 15:46:22 +0900

    :sparkles: Feat: EditComment 조회 API 연결 구현
    Feat: EditComment 조회 API 연결 구현

--- app/project/[projectId]/_components/CommentSection/MyComment/EditComment.tsx
+++ app/project/[projectId]/_components/CommentSection/MyComment/EditComment.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { MyCommentResponse } from "@/app/_apis/schema/commentResponse";
+import { projectQueryKeys } from "@/app/_queryFactory/projectQuery";
 import ToolTip from "../../Comment/ToolTip";
 import EnterRating from "../../Comment/EnterRating";
 import EnterCommentProvider from "../../../_context/EnterCommentProvider";
@@ -7,8 +10,28 @@ import EnterText from "../../Comment/EnterText";
 import EnterButton from "../../Comment/EnterButton";
 import { useMyCommentContext } from "../../../_context/MyCommentProvider";
 
-function EditComment() {
+interface Props {
+  projectId: number;
+  myComment: MyCommentResponse;
+}
+
+function EditComment({ projectId, myComment }: Props) {
   const { setView } = useMyCommentContext();
+  const { data: ratingsData } = useQuery(
+    projectQueryKeys.ratings(projectId, Number(myComment.projectCommentResponseDto?.authorId))
+  );
+
+  if (!ratingsData) return null;
+
+  const transformedData = [
+    ratingsData.ideaRank,
+    ratingsData.designRank,
+    ratingsData.functionRank,
+    ratingsData.completionRank,
+  ];
+
+  if (!myComment.projectCommentResponseDto) return null;
+  const { comment } = myComment.projectCommentResponseDto;
 
   return (
     <div className="relative flex flex-col rounded-xl border border-solid border-gray-300 bg-gray-100 p-6">
@@ -18,8 +41,8 @@ function EditComment() {
       </div>
       <div className="flex flex-col gap-6">
         <EnterCommentProvider>
-          <EnterRating ratingValue={[1, 2, 3, 4]} />
-          <EnterText commentValue="테스트중" />
+          <EnterRating ratingValue={transformedData} />
+          <EnterText commentValue={comment} />
           <EnterButton mode="edit" onClick={() => setView("show")} />
         </EnterCommentProvider>
       </div>
