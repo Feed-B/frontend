933937272b3c341b244509ded4f50729d92c0b05
thgus5335 <thgus5335@naver.com>
2024-06-24 16:12:25 +0900

    :art: Style: CommentProfile 작성자 직무 한글로 변경
    Style: CommentProfile 작성자 직무 한글로 변경

--- app/project/[projectId]/_components/Comment/CommentProfile.tsx
+++ app/project/[projectId]/_components/Comment/CommentProfile.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Link from "next/link";
 import ProfileImage from "@/app/_components/ProfileImage/ProfileImage";
+import { JOB_CATEGORIES_KR } from "@/app/_constants/JobCategoryData";
 
 interface CommentProfileProps {
   userId: number;
   userName: string;
   userJob: string;
 }
+
+type JobCategory = keyof typeof JOB_CATEGORIES_KR;
 
 function CommentProfile({ userId, userName, userJob }: CommentProfileProps) {
   return (
@@ -15,7 +18,7 @@ function CommentProfile({ userId, userName, userJob }: CommentProfileProps) {
         <ProfileImage imageUrl="default" className="h-10 w-10" />
         <div>
           <p className="text-sm font-semibold">{userName}</p>
-          <p className="text-[10px] text-blue-500">{userJob}</p>
+          <p className="text-[10px] text-blue-500">{JOB_CATEGORIES_KR[userJob as JobCategory]}</p>
         </div>
       </div>
     </Link>
--- app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
+++ app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
@@ -7,9 +7,7 @@ import kebabIcon from "@/public/icons/kebab.svg";
 import DropDown from "@/app/_components/DropDown/DropDown";
 import useToggleHook from "@/app/_hooks/useToggleHook";
 import arrowIcon from "@/public/icons/blackArrowRight.svg";
-
 import { MyCommentResponse } from "@/app/_apis/schema/commentResponse";
-import { JOB_CATEGORIES_KR } from "@/app/_constants/JobCategoryData";
 import TotalStar from "../../Comment/TotalStar";
 import { useMyCommentContext } from "../../../_context/MyCommentProvider";
 import CommentProfile from "../../Comment/CommentProfile";
@@ -19,8 +17,6 @@ interface Props {
   projectId: number;
   myComment: MyCommentResponse;
 }
-
-type JobCategory = keyof typeof JOB_CATEGORIES_KR;
 
 function ShowComment({ projectId, myComment }: Props) {
   const { isOpen, toggleState } = useToggleHook();
@@ -35,7 +31,7 @@ function ShowComment({ projectId, myComment }: Props) {
       <h3 className="mb-4 text-lg font-semibold">내가 쓴 글</h3>
       <div className="relative flex flex-col gap-4 rounded-xl border border-solid border-gray-300 bg-gray-100 p-4">
         <div className="flex justify-between">
-          <CommentProfile userId={authorId} userName={authorName} userJob={JOB_CATEGORIES_KR[job as JobCategory]} />
+          <CommentProfile userId={authorId} userName={authorName} userJob={job} />
           <div className="flex items-center gap-2">
             <CommentCount size="large" commentCount={childCommentCount} />
             <Image className="relative" src={kebabIcon} alt="댓글 메뉴." width={24} onClick={toggleState} />
