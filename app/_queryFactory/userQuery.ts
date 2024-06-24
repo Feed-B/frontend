f17560e350ea51d3b7c256f5e6146b9fa9050561
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-06-24 14:24:49 +0900

    ✨ Feat: 헤더 드롭박스 마이페이지 버튼에 userId 연결
    feat: 헤더 드롭박스 마이페이지 버튼에 userId 연결

---
+++ app/_queryFactory/userQuery.ts
@@ -0,0 +1,13 @@
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+import { profileAPI } from "../_apis/ProfileAPI";
+
+export const userQueryKeys = createQueryKeys("user", {
+  userId: () => ({
+    queryKey: ["id"],
+    queryFn: () => profileAPI.getCurrentUserId(),
+  }),
+  detail: (userId: number) => ({
+    queryKey: ["profile"],
+    queryFn: () => profileAPI.getUserData({ userId }),
+  }),
+});
