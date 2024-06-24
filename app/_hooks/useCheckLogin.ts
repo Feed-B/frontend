3596412eab6b7e60ca813cda6c5615bd5d9f4508
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-24 15:18:46 +0900

    ♻️ Refactor: checkLogin hook 구현

---
+++ app/_hooks/useCheckLogin.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLogin } from "../_context/LoginProvider";
+import { getToken, removeToken } from "../_utils/handleToken";
+
+const useCheckLogin = () => {
+  const { type, setType } = useLogin();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token && token.accessToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false); // 로그인 상태 없을 때 업데이트
+    }
+  }, [setType]);
+
+  const handleLogout = () => {
+    removeToken();
+    setType("");
+    setIsLoggedIn(false);
+  };
+
+  return { type, setType, isLoggedIn, handleLogout };
+};
+
+export default useCheckLogin;
