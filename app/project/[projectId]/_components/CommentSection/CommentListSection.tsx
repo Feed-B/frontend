779a951566aa9d728dc82c0173d91cde2c54b6cd
thgus5335 <thgus5335@naver.com>
2024-06-24 23:18:52 +0900

    :art: Style: useCheckLogin 훅 사용
    Style: useCheckLogin 훅 사용

--- app/project/[projectId]/_components/CommentSection/CommentListSection.tsx
+++ app/project/[projectId]/_components/CommentSection/CommentListSection.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import Pagination from "../Comment/Pagination";
+"use client";
 
-const isLogin = true;
+import React from "react";
+import useCheckLogin from "@/app/_hooks/useCheckLogin";
+import Pagination from "../Comment/Pagination";
 
 interface Props {
   projectId: number;
 }
 
 function CommentListSection({ projectId }: Props) {
+  const { isLoggedIn } = useCheckLogin();
   return (
     <>
-      <section className={`mt-10 ${!isLogin && "blur-sm"}`}>
+      <section className={`mt-10 ${!isLoggedIn && "blur-sm"}`}>
         <h3 className="mb-4 text-lg font-bold text-gray-900">댓글</h3>
         <Pagination projectId={projectId} />
-        {!isLogin && <div className="absolute left-0 top-0 z-10 h-full w-full" />}
+        {!isLoggedIn && <div className="absolute left-0 top-0 z-10 h-full w-full" />}
       </section>
     </>
   );
