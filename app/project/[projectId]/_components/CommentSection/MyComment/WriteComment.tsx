fa238404424156cf68a85108186bf7dda5a01651
thgus5335 <thgus5335@naver.com>
2024-06-24 23:18:52 +0900

    :art: Style: useCheckLogin 훅 사용
    Style: useCheckLogin 훅 사용

--- app/project/[projectId]/_components/CommentSection/MyComment/WriteComment.tsx
+++ app/project/[projectId]/_components/CommentSection/MyComment/WriteComment.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useCheckLogin from "@/app/_hooks/useCheckLogin";
 import ToolTip from "../../Comment/ToolTip";
 import WriteCommentShield from "../../Shield/WriteCommentShield";
 import EnterRating from "../../Comment/EnterRating";
@@ -8,15 +9,14 @@ import EnterText from "../../Comment/EnterText";
 import EnterButton from "../../Comment/EnterButton";
 import { useMyCommentContext } from "../../../_context/MyCommentProvider";
 
-const isLogin = true;
-
 function WriteComment() {
   const { setView } = useMyCommentContext();
+  const { isLoggedIn } = useCheckLogin();
 
   return (
     <div className="relative flex flex-col rounded-xl border border-solid border-gray-300 bg-gray-100 p-6">
-      {!isLogin && <WriteCommentShield />}
-      <div className={`${!isLogin && "blur-sm"}`}>
+      {!isLoggedIn && <WriteCommentShield />}
+      <div className={`${!isLoggedIn && "blur-sm"}`}>
         <div className="mb-8 flex items-center gap-1">
           <p className="text-xl font-semibold text-gray-900">프로젝트를 평가해주세요</p>
           <ToolTip />
--- app/project/[projectId]/_components/CommentSection/MyCommentSection.tsx
+++ app/project/[projectId]/_components/CommentSection/MyCommentSection.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { MyCommentResponse } from "@/app/_apis/schema/commentResponse";
-import { commentQueryKeys } from "@/app/_queryFactory/commentQuery";
+import { useQuery } from "@tanstack/react-query";
+import useCheckLogin from "@/app/_hooks/useCheckLogin";
+import { commentApi } from "@/app/_apis/comment";
 import MyCommentProvider from "../../_context/MyCommentProvider";
 import { useMyCommentContext } from "../../_context/MyCommentProvider";
 import ShowComment from "./MyComment/ShowComment";
@@ -16,14 +16,20 @@ interface Props {
 
 const CommentContainer = ({ projectId }: Props) => {
   const { view, setView } = useMyCommentContext();
-  const { data: myComment }: UseQueryResult<MyCommentResponse, Error> = useQuery(commentQueryKeys.myComment(projectId));
-  useEffect(() => {
-    if (myComment && myComment.exists) {
-      setView("show");
-    }
-  }, [myComment, setView]);
+  const { isLoggedIn } = useCheckLogin();
 
-  if (!myComment) return null;
+  const { data: myComment } = useQuery({
+    queryKey: ["myComment"],
+    queryFn: () => commentApi.getMyComment(projectId),
+    enabled: !!isLoggedIn,
+  });
+
+  useEffect(() => {
+    if (myComment && myComment.exists) setView("show");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [myComment]);
+
+  if (!myComment) return <WriteComment />;
 
   return (
     <section>
