7e6de90951599fc8d705a1882b4ed4d938ce4adc
yu kyoung kim <sksms2104@naver.com>
2024-06-24 15:34:03 +0900

    🎨 Style: header 공통 HEADER 사용

--- app/_apis/handleLikeProject.ts
+++ app/_apis/handleLikeProject.ts
@@ -1,11 +1,11 @@
+import { HEADER } from "../_constants/HeaderToken";
 import httpClient from "./httpClient";
-import { headers } from "./projectListAPI";
 
 export const handleLikeProject = {
   postLikeProject: async ({ projectId }: { projectId: number }) => {
-    return await httpClient().post(`/projects/${projectId}/like`, { "": "" }, headers.headers);
+    return await httpClient().post(`/projects/${projectId}/like`, { "": "" }, HEADER.headers);
   },
   deleteLikeProject: async ({ projectId }: { projectId: number }) => {
-    return await httpClient().delete(`/projects/${projectId}/unlike`, headers.headers);
+    return await httpClient().delete(`/projects/${projectId}/unlike`, HEADER.headers);
   },
 };
