92f4d2db1bc311da0509eae24bc9f7d636feec51
JeongSabibi <jsbb528@naver.com>
2024-06-24 13:46:36 +0900

    🎨 Style: 임시 header 삭제 및 header token 추가
    Style: 임시 header 삭제 및 header token 추가

--- app/_apis/addProjectApi.ts
+++ app/_apis/addProjectApi.ts
@@ -1,13 +1,8 @@
+import { HEADER } from "@/app/_constants/HeaderToken";
 import httpClient from "./httpClient";
 
 export const addProjectApi = {
   postProject: async (projectData: FormData) => {
-    return await httpClient().postFormData("/projects", projectData, headers);
+    return await httpClient().postFormData("/projects", projectData, HEADER.headers);
   },
-};
-
-// 임시 headers
-export const headers = {
-  Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5OTNhNmEyMSIsImlhdCI6MTcxOTE0NDQ5NiwiZXhwIjoxNzE5MTY2MDk2fQ.7XwJLTQmT0G0nJMwEDg8zg69JSMeeayo8FxFf3COjVo",
 };
