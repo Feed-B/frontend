5d5628aab311d3bca40787ecb28577b7adf917fe
thgus5335 <thgus5335@naver.com>
2024-06-24 15:21:38 +0900

    :sparkles: Feat: ShowComment API 연결 구현
    Feat: ShowComment API 연결 구현

--- app/project/[projectId]/_components/Comment/TotalStar.tsx
+++ app/project/[projectId]/_components/Comment/TotalStar.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import React from "react";
 import starIcon from "@/public/icons/fullStar.svg";
 
-function TotalStar() {
+interface Props {
+  starRating: number;
+}
+
+function TotalStar({ starRating }: Props) {
   return (
     <div className="flex items-center gap-1">
       <Image src={starIcon} alt="총 별점." width={24} />
-      <p className="text-sm font-semibold text-gray-900">3.5</p>
+      <p className="text-sm font-semibold text-gray-900">{starRating}</p>
     </div>
   );
 }
--- app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
+++ app/project/[projectId]/_components/CommentSection/MyComment/ShowComment.tsx
@@ -7,21 +7,35 @@ import DropDown from "@/app/_components/DropDown/DropDown";
 import useToggleHook from "@/app/_hooks/useToggleHook";
 import arrowIcon from "@/public/icons/blackArrowRight.svg";
 
+import { MyCommentResponse } from "@/app/_apis/schema/commentResponse";
+import { JOB_CATEGORIES_KR } from "@/app/_constants/JobCategoryData";
 import TotalStar from "../../Comment/TotalStar";
 import { useMyCommentContext } from "../../../_context/MyCommentProvider";
+import CommentProfile from "../../Comment/CommentProfile";
+import CommentCount from "../../Comment/CommentCount";
 
-function ShowComment() {
+interface Props {
+  myComment: MyCommentResponse;
+}
+
+type JobCategory = keyof typeof JOB_CATEGORIES_KR;
+
+function ShowComment({ myComment }: Props) {
   const { isOpen, toggleState } = useToggleHook();
   const { setView } = useMyCommentContext();
+
+  if (!myComment.projectCommentResponseDto) return null;
+  const { authorId, authorName, job, childCommentCount, comment, averageStarRank } =
+    myComment.projectCommentResponseDto;
 
   return (
     <>
       <h3 className="mb-4 text-lg font-semibold">내가 쓴 글</h3>
       <div className="relative flex flex-col gap-4 rounded-xl border border-solid border-gray-300 bg-gray-100 p-4">
         <div className="flex justify-between">
-          {/* <CommentProfile /> */}
+          <CommentProfile userId={authorId} userName={authorName} userJob={JOB_CATEGORIES_KR[job as JobCategory]} />
           <div className="flex items-center gap-2">
-            {/* <CommentCount size="large" /> */}
+            <CommentCount size="large" commentCount={childCommentCount} />
             <Image className="relative" src={kebabIcon} alt="댓글 메뉴." width={24} onClick={toggleState} />
             {isOpen && (
               <DropDown className="w-fit translate-x-4 translate-y-16">
@@ -31,9 +45,9 @@ function ShowComment() {
             )}
           </div>
         </div>
-        <p className="text-overflow-3 h-14 text-sm text-gray-900">댓글 내용입니다.</p>
+        <p className="text-overflow-3 h-14 text-sm text-gray-900">{comment}</p>
         <div className="flex justify-between">
-          <TotalStar />
+          <TotalStar starRating={averageStarRank} />
           <div className="flex items-center">
             <p className="text-xs text-gray-900">더보기</p>
             <Image src={arrowIcon} alt="댓글 상세보기." width={20} />
