d5220d8bbb74fd217805b5aa9b5d7f0b792cec5d
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-06-24 14:54:22 +0900

    💄 Design: 로딩스피너 디자인 변경

--- app/_components/LoadingWrapper/LoadingWrapper.tsx
+++ app/_components/LoadingWrapper/LoadingWrapper.tsx
@@ -2,11 +2,12 @@
 
 import Image from "next/image";
 import { ReactNode, useEffect, useState } from "react";
-import { BounceLoader } from "react-spinners";
+import { BeatLoader } from "react-spinners";
 import feedbee from "@/public/icons/feedbee.svg";
 
 interface props {
   children?: ReactNode;
+  loadingTime?: number;
 }
 
 function LoadingWrapper({ children }: props) {
@@ -15,12 +16,13 @@ function LoadingWrapper({ children }: props) {
   useEffect(() => {
     setIsLoading(false); // 로딩 상태 해제
   }, []);
+
   return (
     <>
       {isLoading ? (
         <div className="flex h-screen flex-col items-center justify-center gap-4">
           <Image src={feedbee} alt="피드비 로고" priority />
-          <BounceLoader loading={true} size={60} color="#FDCB07" />
+          <BeatLoader loading={true} color="#FDCB07" speedMultiplier={5} />
         </div>
       ) : (
         children
