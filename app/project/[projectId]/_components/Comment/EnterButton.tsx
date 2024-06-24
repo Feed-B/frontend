5104fcdc29c2b54c19b7fd6eb5fd47e698cf728d
thgus5335 <thgus5335@naver.com>
2024-06-25 00:07:55 +0900

    :bug: Fix: 주석 처리
    Fix: 주석 처리

--- app/project/[projectId]/_components/Comment/EnterButton.tsx
+++ app/project/[projectId]/_components/Comment/EnterButton.tsx
@@ -1,42 +1,39 @@
 "use client";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Button from "@/app/_components/Button/Button";
-import { commentApi } from "@/app/_apis/comment";
-import { useEnterCommentContext } from "../../_context/EnterCommentProvider";
 
 interface Props {
-  projectId: number;
+  // projectId: number;
   mode?: "write" | "edit";
   onClick: () => void;
 }
 
-function EnterButton({ projectId, mode = "write", onClick }: Props) {
-  const { rating, comment } = useEnterCommentContext();
-  const queryClient = useQueryClient();
+function EnterButton({ mode = "write", onClick }: Props) {
+  // const { rating, comment } = useEnterCommentContext();
+  // const queryClient = useQueryClient();
 
-  const mutation = useMutation({
-    mutationFn: (comment: string) => {
-      return commentApi.postComment(projectId, rating, comment);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["comment", "list", "commentList"],
-      });
-    },
-  });
+  // const mutation = useMutation({
+  //   mutationFn: (comment: string) => {
+  //     return commentApi.postComment(projectId, rating, comment);
+  //   },
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({
+  //       queryKey: ["comment", "list", "commentList"],
+  //     });
+  //   },
+  // });
 
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    if (rating.every(element => element > 0)) {
-      mutation.mutate(comment);
-    }
-  };
+  // const handleSubmit = (event: { preventDefault: () => void }) => {
+  //   event.preventDefault();
+  //   if (rating.every(element => element > 0)) {
+  //     mutation.mutate(comment);
+  //   }
+  // };
 
   return (
     <div className="flex">
       {mode === "write" ? (
-        <Button className="ml-auto" buttonSize="normal" bgColor="yellow" onClick={handleSubmit}>
+        <Button className="ml-auto" buttonSize="normal" bgColor="yellow" onClick={onClick}>
           등록
         </Button>
       ) : (
