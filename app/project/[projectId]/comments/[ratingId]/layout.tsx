85974827fc25857b5c5f30babb04188c5b7c1f4d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-07-13 22:19:02 +0900

    ✨ Feat: 메타태그 설정

--- app/project/[projectId]/comments/[ratingId]/layout.tsx
+++ app/project/[projectId]/comments/[ratingId]/layout.tsx
@@ -1,23 +1,36 @@
 import "@/app/_styles/globals.css";
-// import { commentApi } from "@/app/_apis/comment";
+import { commentApi } from "@/app/_apis/comment";
+import { projectApi } from "@/app/_apis/project";
 
 interface Props {
   params: {
-    commentId: number;
+    ratingId: number;
     projectId: number;
   };
   children: React.ReactNode;
 }
 
-// export async function generateMetadata({ params }: Props) {
-//   const api = () => commentApi.getCommentDetail(params.projectId, params.commentId);
+export async function generateMetadata({ params }: Props) {
+  const projectData = await projectApi.getProject(params.projectId);
+  const commentData = await commentApi.getCommentDetail(params.ratingId);
 
-//   const result = await api();
-//   console.log(result);
-//   return {
-//     title: "...",
-//   };
-// }
+  return {
+    title: `${commentData.authorName} | 피드백 `,
+    openGraph: {
+      title: `${projectData.title}`,
+      description: `${projectData.content}`,
+      url: "https://feedb.vercel.app/",
+      siteName: "FeedB",
+      images: [
+        {
+          url: projectData.thumbnailUrl,
+          width: 500,
+          height: 500,
+        },
+      ],
+    },
+  };
+}
 
 export default function CommentLayout({ children }: Readonly<Props>) {
   return <>{children}</>;
