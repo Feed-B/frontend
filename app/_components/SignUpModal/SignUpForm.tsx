15725b6a2704712a06936b16e6dc05ae309895a7
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-14 17:26:29 +0900

    💄 회원가입모달 textarea 수정

--- app/_components/SignUpModal/SignUpForm.tsx
+++ app/_components/SignUpModal/SignUpForm.tsx
@@ -119,7 +119,7 @@ function SignUpForm({ item, dataType, setItem, handleModalClose }: SignUpFormPro
               message: `소개는 ${CONSTANTS.INTRODUCTION_MAX_LENGTH}자를 초과할 수 없습니다`,
             },
           })}
-          className="h-40 w-96 rounded-lg border border-gray-200 px-2 py-2 text-sm focus:border-gray-900 focus:outline-none"
+          className="h-40 w-96 resize-none rounded-lg border border-gray-200 px-2 py-2 text-sm focus:border-gray-900 focus:outline-none"
         />
       </div>
 
