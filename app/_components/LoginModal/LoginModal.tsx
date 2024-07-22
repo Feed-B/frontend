55c7bf9eb1e6ddcde93ae3c024344629ac76f9d6
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-23 02:27:53 +0900

    💄 Design: loginModal 반응형 UI

--- app/_components/LoginModal/LoginModal.tsx
+++ app/_components/LoginModal/LoginModal.tsx
@@ -27,22 +27,16 @@ function LoginModal({ openModal, handleModalClose }: LoginModalProps) {
   };
 
   return (
-    <Modal openModal={openModal} handleModalClose={handleModalClose} className="max-h-[612px] w-[432px] p-6">
+    <Modal
+      openModal={openModal}
+      handleModalClose={handleModalClose}
+      className="max-h-[612px] w-[432px] p-6 mb:w-[360px] mb:p-5">
       <div className="flex flex-col items-center">
         <div className="mt-[73px]">
           <Image src={logoTextIcon} width={180} className="mb-6" alt="로고 텍스트" />
         </div>
 
         <Image src={feedbee} width={180} height={175} className="mb-[41px]" alt="피드비" priority />
-
-        {/* <Image
-          src={dottedLine}
-          width={679.047}
-          height={252.715}
-          className="absolute bottom-[115px] -z-10"
-          alt="점선"
-          priority
-        /> */}
 
         <p className="mb-6 text-xl font-semibold">
           <span className="text-blue-500">로그인</span>이 필요한 서비스입니다
@@ -52,7 +46,7 @@ function LoginModal({ openModal, handleModalClose }: LoginModalProps) {
           buttonSize="normal"
           bgColor="naver"
           onClick={handleNaverLogin}
-          className="mb-2 flex h-[52px] w-96 items-center justify-center gap-3 text-lg font-medium">
+          className="mb-2 flex h-[52px] w-96 items-center justify-center gap-3 text-lg font-medium mb:w-[335px]">
           <Image src={naverIcon} alt="네이버 아이콘" width={20} height={20} priority />
           네이버 로그인
         </Button>
@@ -61,7 +55,7 @@ function LoginModal({ openModal, handleModalClose }: LoginModalProps) {
           buttonSize="normal"
           bgColor="kakao"
           onClick={handleKakaoLogin}
-          className="flex h-[52px] w-96 items-center justify-center gap-3 bg-[#FEE500] text-lg font-medium">
+          className="flex h-[52px] w-96 items-center justify-center gap-3 bg-[#FEE500] text-lg font-medium mb:w-[335px]">
           <Image src={kakaoIcon} alt="카카오 아이콘" width={20} height={20} priority />
           카카오 로그인
         </Button>
