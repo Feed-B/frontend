6e69f92646cf02e10b61937e456e9ae1bc5561cd
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-07-27 00:21:47 +0900

    ♻️ Refactor: 현재 페이지 위치를 로그인 모달에서 가져올 수 있도록 변경
    refactor: 현재 페이지 위치를 로그인 모달에서 가져올 수 있도록 변경1

--- app/_components/LoginButton/LoginButton.tsx
+++ app/_components/LoginButton/LoginButton.tsx
@@ -4,12 +4,12 @@ import useModal from "@/app/_hooks/useModal";
 import LoginModal from "@/app/_components/LoginModal/LoginModal";
 import Button from "../Button/Button";
 
-function LoginButton({ pathName }: { pathName: string }) {
+function LoginButton() {
   const { openModal: loginModal, handleModalClose: loginModalClose, handleModalOpen: loginModalOpen } = useModal();
 
   return (
     <>
-      <LoginModal pathName={pathName} openModal={loginModal} handleModalClose={loginModalClose} />
+      <LoginModal openModal={loginModal} handleModalClose={loginModalClose} />
 
       <Button buttonSize="normal" bgColor="yellow" onClick={loginModalOpen}>
         로그인
