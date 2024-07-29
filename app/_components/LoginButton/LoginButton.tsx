b9a96ac8ae24115f5b1ee0bfd8c9f39e53036b89
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-29 17:02:49 +0900

    ✏️ Fix: 로그인 후 success 페이지에 머물러있는 오류 해결

--- app/_components/LoginButton/LoginButton.tsx
+++ app/_components/LoginButton/LoginButton.tsx
@@ -1,17 +1,31 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { useLogin } from "@/app/_context/LoginProvider";
 import useModal from "@/app/_hooks/useModal";
 import LoginModal from "@/app/_components/LoginModal/LoginModal";
 import Button from "../Button/Button";
 
 function LoginButton() {
   const { openModal: loginModal, handleModalClose: loginModalClose, handleModalOpen: loginModalOpen } = useModal();
+  const pathName = usePathname();
+
+  const { setUrl, url } = useLogin();
+
+  const handleClick = () => {
+    setUrl(pathName);
+
+    loginModalOpen();
+  };
+
+  useEffect(() => {}, [url]);
 
   return (
     <>
       <LoginModal openModal={loginModal} handleModalClose={loginModalClose} />
 
-      <Button buttonSize="normal" bgColor="yellow" onClick={loginModalOpen}>
+      <Button buttonSize="normal" bgColor="yellow" onClick={handleClick}>
         로그인
       </Button>
     </>
