82afa03e83978542a780a93b3438706436d53422
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-08-01 19:24:52 +0900

    ✨ Feat: 로그인시 토큰 전역상태로 관리

--- app/auth/success/_components/Success.tsx
+++ app/auth/success/_components/Success.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { useEffect } from "react";
-import { setToken } from "@/app/_utils/handleToken";
+import { setToken as setLocalStorageToken } from "@/app/_utils/handleToken";
 import LoadingWrapper from "@/app/_components/LoadingWrapper/LoadingWrapper";
 import { useLogin } from "@/app/_context/LoginProvider";
 
@@ -18,7 +18,7 @@ export default function Success() {
 
 function SuccessContent({ router, pathname }: any) {
   const searchParams = useSearchParams();
-  const { url, setEmail, setType } = useLogin();
+  const { url, setEmail, setType, setToken } = useLogin();
 
   useEffect(() => {
     const typeQuery = searchParams.get("type");
@@ -36,7 +36,9 @@ function SuccessContent({ router, pathname }: any) {
     // 토큰이 있으면 로컬스토리지에 저장
     if (tokenQuery) {
       const accessToken = tokenQuery;
+      // 토큰을 전역으로 관리
       setToken(accessToken);
+      setLocalStorageToken(accessToken);
       const timestamp = Date.now();
       localStorage.setItem("tokenTimestamp", timestamp.toString());
     }
