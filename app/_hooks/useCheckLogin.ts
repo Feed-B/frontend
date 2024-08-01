7e9a3808d5867d2f9186a61940ddba2c09a83064
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-08-01 19:25:56 +0900

    🐛 Fix: 전역상태로 관리된 token을 이용한 로그인 유무

--- app/_hooks/useCheckLogin.ts
+++ app/_hooks/useCheckLogin.ts
@@ -1,19 +1,10 @@
 import { useEffect, useState } from "react";
 import { useLogin } from "../_context/LoginProvider";
-import { checkTokenExpiry, getToken, removeToken } from "../_utils/handleToken";
+import { checkTokenExpiry, removeToken } from "../_utils/handleToken";
 
 const useCheckLogin = () => {
   const { type, setType } = useLogin();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const token = getToken();
-    if (token && token.accessToken) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false); // 로그인 상태 없을 때 업데이트
-    }
-  }, [setType]);
 
   useEffect(() => {
     checkTokenExpiry();
