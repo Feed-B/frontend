49392c735c9afbb8d37221fb78a6777593ebc9a8
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-07 01:11:00 +0900

    🐛 Fix: openGraph 이미지 변경
    Fix: openGraph 이미지 변경

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     siteName: "FeedB",
     images: [
       {
-        url: "/images/openGraph.svg",
+        url: "/images/openGraph.png",
         width: 1920,
         height: 1080,
       },
