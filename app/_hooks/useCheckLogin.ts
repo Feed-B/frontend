31d58fad039121e18710b573b4fcf74fc662b21e
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-08-07 22:38:19 +0900

    ✨ Feat: 로그아웃시 메인페이지로 이동

--- app/_hooks/useCheckLogin.ts
+++ app/_hooks/useCheckLogin.ts
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useLogin } from "../_context/LoginProvider";
-import { checkTokenExpiry, removeRedirectUrl, removeToken } from "../_utils/handleToken";
+import { checkTokenExpiry, getToken, removeRedirectUrl, removeToken } from "../_utils/handleToken";
 
 const useCheckLogin = () => {
-  const { type, setType } = useLogin();
+  const router = useRouter();
+  const { type, setType, token } = useLogin();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const accessToken = getToken();
+    if (token || (accessToken && accessToken.accessToken)) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [token, setIsLoggedIn]);
 
   useEffect(() => {
     checkTokenExpiry();
@@ -15,6 +26,7 @@ const useCheckLogin = () => {
     setType("");
     setIsLoggedIn(false);
     removeRedirectUrl();
+    router.push("/main");
   };
 
   return { type, setType, isLoggedIn, handleLogout, setIsLoggedIn };
