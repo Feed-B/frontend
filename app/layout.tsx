bb5c0aee8b0a0ac1148d132b2400676901a1c5c4
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-08 17:10:48 +0900

    🔧 Chore: 메타태그 description 수정
    chore: 메타태그 description 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./_styles/globals.css";
 export const metadata: Metadata = {
   title: "피드비",
   description:
-    "프로젝트를 진행하면서 종종 의견을 얻고 싶거나, 다른 사람들의 피드백을 받고 싶었던 적이 있으신가요? FeedB는 여러분의 프로젝트를 공유하고, 서로 피드백을 주고받을 수 있는 최적의 플랫폼입니다. FeedB를 통해 프로젝트의 완성도를 높이고, 새로운 아이디어를 얻어보세요!",
+    "FeedB는 여러분의 프로젝트를 공유하고, 서로 피드백을 주고받을 수 있는 최적의 플랫폼입니다. FeedB를 통해 프로젝트의 완성도를 높이고, 새로운 아이디어를 얻어보세요!",
   icons: {
     icon: "/beeIcons/yellowBee.svg",
   },
