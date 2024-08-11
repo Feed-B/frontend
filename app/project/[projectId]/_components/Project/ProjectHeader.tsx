b49e457143d99ddfd0479c3a96cae5de7d3291b2
thgus5335 <thgus5335@naver.com>
2024-08-12 04:15:03 +0900

    :bug: Fix: useEffect 의존값 추가
    Fix: useEffect 의존값 추가

--- app/project/[projectId]/_components/Project/ProjectHeader.tsx
+++ app/project/[projectId]/_components/Project/ProjectHeader.tsx
@@ -49,8 +49,6 @@ function ProjectHeader({ projectId }: Props) {
   const { data: project }: UseQueryResult<ProjectResponse, Error> = useQuery(projectQueryKeys.detail(projectId));
   if (!project) return null;
 
-  console.log("project.isMine", project.isMine);
-
   const handleDeleteModal = () => {
     openDeleteModal();
   };
