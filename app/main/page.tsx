68c7f964a7ff32913928a120e0907ea55ba92e39
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-12 11:25:49 +0900

    🐛 Fix: 프리페칭 주석 후 좋아요 오류 해결
    Fix: 프리페칭 주석 후 좋아요 오류 해결

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import getQueryClient from "../_queryFactory/getQueryClient";
-import { projectQueryKeys } from "../_queryFactory/projectQuery";
+// import { projectQueryKeys } from "../_queryFactory/projectQuery";
 import { revalidateTagAction } from "../_utils/revalidationAction";
 import SelectStack from "./_components/SelectStack/SelectStack";
 
@@ -9,20 +9,20 @@ async function MainPage() {
   revalidateTagAction("pojectList");
   const queryClient = getQueryClient();
 
-  const projectListQuery = projectQueryKeys.list({ page: 1, size: 16 });
+  // const projectListQuery = projectQueryKeys.list({ page: 1, size: 16 });
 
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: projectListQuery.queryKey,
-    queryFn: projectListQuery.queryFn,
-    initialPageParam: 1 as never,
-    getNextPageParam: (lastPage: any) => {
-      const { customPageable } = lastPage;
-      if (customPageable.hasNext) {
-        return customPageable.page + 1; // 다음 페이지 번호 반환
-      }
-      return undefined; // 더 이상 페이지가 없으면 undefined 반환
-    },
-  });
+  // await queryClient.prefetchInfiniteQuery({
+  //   queryKey: projectListQuery.queryKey,
+  //   queryFn: projectListQuery.queryFn,
+  //   initialPageParam: 1 as never,
+  //   getNextPageParam: (lastPage: any) => {
+  //     const { customPageable } = lastPage;
+  //     if (customPageable.hasNext) {
+  //       return customPageable.page + 1; // 다음 페이지 번호 반환
+  //     }
+  //     return undefined; // 더 이상 페이지가 없으면 undefined 반환
+  //   },
+  // });
 
   const dehydratedState = dehydrate(queryClient);
 
