89751a3a04b1fce46da5849274ecd69c81776b6d
codingaring <sksms2104@naver.com>
2024-08-29 19:41:05 +0900

    💄 Design: 프로필 관련 반응형 구현

--- app/profile/[userId]/_components/MyPageContent.tsx
+++ app/profile/[userId]/_components/MyPageContent.tsx
@@ -23,8 +23,16 @@ function MyPageContent() {
   const isMyPage = currentUserId.id === Number(userId) ? true : false;
 
   return (
-    <main className="mx-auto mt-[64px] grid max-w-[1200px] grid-cols-[180px_1fr] grid-rows-2 gap-x-11 gap-y-8 mb:mt-4 mb:grid-cols-1 mb:grid-rows-[minmax(120px,_1fr)_88px_1fr] mb:gap-y-6 mb:p-5">
-      <div className="col-span-1 row-span-2 mb:row-start-2 mb:row-end-3">
+    <main
+      className="mx-auto mt-[64px] grid max-w-[1200px] grid-cols-[180px_1fr] grid-rows-[186px_1fr] gap-x-11 gap-y-8 
+      mb:mt-4 mb:grid-cols-1 mb:grid-rows-[minmax(120px,_1fr)_88px_1fr] mb:gap-y-6 mb:p-5 
+    tbc:mt-4 tbc:grid-cols-1 tbc:grid-rows-[180px_88px_1fr] tbc:gap-y-10 tbc:p-5
+    tbr:mt-4 tbr:grid-cols-1 tbr:grid-rows-[180px_88px_1fr] tbr:gap-y-10 tbr:p-5">
+      <div
+        className="col-span-1 row-span-2 
+        mb:row-start-2 mb:row-end-3 
+        tbc:row-start-2 tbc:row-end-3
+        tbr:row-start-2 tbr:row-end-3">
         <MyPageCategory
           isMyPage={isMyPage}
           selectCategory={selectCategory}
