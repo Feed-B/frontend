149c97b6458aed454648914a714b630c8bcd3a03
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-15 17:26:35 +0900

    💄 Design: 검색결과 없을 때 나오는 컴포넌트 반응형 추가
    design: 검색결과 없을 때 나오는 컴포넌트 반응형 추가

--- app/_components/ProjectList/ProjectCard/EmptyCard.tsx
+++ app/_components/ProjectList/ProjectCard/EmptyCard.tsx
@@ -8,9 +8,9 @@ function EmptyCard() {
   const { projectState } = useGetStack();
 
   return (
-    <div className="absolute left-1/2 top-10 flex -translate-x-1/2 flex-col items-center justify-center gap-6">
+    <div className="absolute left-1/2 top-10 flex w-full -translate-x-1/2 flex-col items-center justify-center gap-6">
       <Image width={54} height={72} src={emptyFileIcon} alt="파일 없음" />
-      <p className="text-xl font-semibold text-gray-700">
+      <p className="min-w-[300px] text-xl font-semibold text-gray-700">
         {projectState.searchString ? (
           <>
             <span className="text-xl font-semibold text-blue-600">{projectState.searchString}</span>에 대한 검색결과가
