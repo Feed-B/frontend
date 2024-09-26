077da1716ee09195b29d6304f777abec7e5eeb84
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-26 23:24:05 +0900

    ♻️ Refactor: 프로젝트 관련 query key 정리
    Refactor: 프로젝트 관련 query key 정리

--- app/_queryFactory/projectQuery.ts
+++ app/_queryFactory/projectQuery.ts
@@ -1,9 +1,7 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
-import { dehydrate } from "@tanstack/react-query";
 import { projectApi } from "../_apis/projectApi";
 import { projectListApi } from "../_apis/projectListApi";
 import { ProjectListParams } from "../_types/ProjectListType";
-import getQueryClient from "./getQueryClient";
 
 export const projectQueryKeys = createQueryKeys("project", {
   list: (props: ProjectListParams, token?: string) => ({
@@ -24,29 +22,10 @@ export const projectQueryKeys = createQueryKeys("project", {
   }),
 });
 
-//프로젝트 키는 이렇게 바꿀겁니다
-export const newProjectQueryKeys = createQueryKeys("project", {
-  list: (props: ProjectListParams) => ["projectList", props],
+export const projectQueryKey = createQueryKeys("project", {
+  list: () => ["projectList"],
+  detail: (projectId: number) => ["projectDetail", projectId],
+  edit: () => ["eidtProject"],
+  myProject: () => ["myProjectList"],
+  likeProject: () => ["LikeProjectList"],
 });
-
-// 이렇게 프리페칭 코드를 작성하고 훅으로 따로 뺄겁니다
-export const usePrefetchQuery = async (params: ProjectListParams) => {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: newProjectQueryKeys.list(params).queryKey,
-    queryFn: ({ pageParam = 1 }) => projectListApi.getProjectList({ ...params, page: pageParam }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage: any) => {
-      const { customPageable } = lastPage;
-      if (customPageable.hasNext) {
-        return customPageable.page + 1; // 다음 페이지 번호 반환
-      }
-      return undefined; // 더 이상 페이지가 없으면 undefined 반환
-    },
-  });
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return { dehydratedState };
-};
