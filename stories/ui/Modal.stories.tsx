31c267b33b94c3e132b8d6bf2b488708b398d11c
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-11-03 17:30:59 +0900

    Feat: 회원가입, 프로필 수정 모달 스토리북 생성

--- stories/ui/Modal.stories.tsx
+++ stories/ui/Modal.stories.tsx
@@ -4,6 +4,11 @@ import Modal from "@/app/_components/Modal/Modal";
 import LoginModal from "@/app/_components/Modal/LoginModal";
 import Button from "@/app/_components/Button/Button";
 import useModal from "@/app/_hooks/useModal";
+import SignUpModal from "@/app/_components/Modal/SignUpModal/SignUpModal";
+import { LoginProvider } from "@/app/_context/LoginProvider";
+import Providers from "@/app/_context/queryProviders";
+import { UserResponse } from "@/app/_apis/schema/userResponse";
+import EditProfileModal from "@/app/profile/[userId]/_components/EditProfileModal/EditProfileModal";
 
 const meta: Meta<typeof Modal> = {
   component: Modal,
@@ -33,6 +38,67 @@ const LoginModalComponents = () => {
   );
 };
 
+const SignupModalComponents = () => {
+  const { openModal, handleModalClose, handleModalOpen } = useModal();
+
+  const handleClick = () => {
+    handleModalOpen();
+  };
+
+  return (
+    <>
+      <SignUpModal openModal={openModal} handleModalClose={handleModalClose} dataType="jop" />
+      <Button buttonSize="normal" bgColor="yellow" onClick={handleClick}>
+        회원가입
+      </Button>
+    </>
+  );
+};
+
+const ProfileModalComponents = () => {
+  const userProfileData: UserResponse = {
+    id: 1,
+    email: "ywy040150@gmail.com",
+    nickName: "피드비 개발자",
+    aboutMe: "피드비 디자인 시스템",
+    job: "FRONTEND",
+    imageUrl: "",
+  };
+
+  const { openModal, handleModalClose, handleModalOpen } = useModal();
+
+  const handleClick = () => {
+    handleModalOpen();
+  };
+
+  return (
+    <>
+      <EditProfileModal profileData={userProfileData} openModal={openModal} handleModalClose={handleModalClose} />
+      <Button onClick={handleClick} type="button" bgColor="stroke" buttonSize="normal">
+        수정
+      </Button>
+    </>
+  );
+};
+
 export const LoginModalStory: Story = {
   render: () => <LoginModalComponents />,
+};
+
+export const SignupModalStory: Story = {
+  render: () => (
+    <Providers>
+      <LoginProvider>
+        <SignupModalComponents />
+      </LoginProvider>
+    </Providers>
+  ),
+};
+
+export const ProfileModalStory: Story = {
+  render: () => (
+    <Providers>
+      <ProfileModalComponents />
+    </Providers>
+  ),
 };
