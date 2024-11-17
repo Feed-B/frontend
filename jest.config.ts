b236725e0e3dd26db3fe9ccd3728013227cc5411
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-11-17 21:10:22 +0900

    🧪 Test: 모달 테스트 코드 작성
    test: 모달 테스트 코드 작성

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
